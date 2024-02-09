0548010d388184ff7a5ed60a63a996d49637c76f
Ryan Andriamahery <ryan@hei.school>
2024-02-09 09:58:23 +0300

    chore: add annotator api url inside test environment registry

---
+++ src/test/java/app/bpartners/geojobs/conf/AnnotatorApiConf.java
@@ -0,0 +1,11 @@
+package app.bpartners.geojobs.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class AnnotatorApiConf {
+    void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "annotator.api.url",
+                () -> "dummy");
+    }
+}
