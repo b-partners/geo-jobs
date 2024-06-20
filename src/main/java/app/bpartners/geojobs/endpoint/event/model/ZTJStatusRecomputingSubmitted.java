92310b5f727698a7ad7ade023fcaa18736c0a90e
Ryan Andriamahery <ryan@hei.school>
2024-06-20 14:08:29 +0300

    chore: handle ZTJStatusRecomputingSubmitted event into event_stack_2

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/ZTJStatusRecomputingSubmitted.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/ZTJStatusRecomputingSubmitted.java
@@ -3,6 +3,8 @@ package app.bpartners.geojobs.endpoint.event.model;
 import static java.time.temporal.ChronoUnit.MINUTES;
 
 import java.time.Duration;
+
+import app.bpartners.geojobs.endpoint.event.EventStack;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,5 +30,10 @@ public class ZTJStatusRecomputingSubmitted extends PojaEvent {
   @Override
   public Duration maxConsumerBackoffBetweenRetries() {
     return Duration.of(1, MINUTES);
+  }
+
+  @Override
+  public EventStack getEventStack() {
+    return EventStack.EVENT_STACK_2;
   }
 }
