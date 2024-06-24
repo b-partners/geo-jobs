59a0c197b790106ba86d10226e00408bc62a2dd0
Ryan Andriamahery <ryan@hei.school>
2024-06-24 15:09:46 +0300

    chore: add ParcelDetectionJobCreatedService logs

--- src/main/java/app/bpartners/geojobs/service/event/ParcelDetectionJobCreatedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/ParcelDetectionJobCreatedService.java
@@ -5,14 +5,17 @@ import app.bpartners.geojobs.endpoint.event.model.ParcelDetectionJobCreated;
 import app.bpartners.geojobs.endpoint.event.model.TileDetectionTaskCreated;
 import app.bpartners.geojobs.repository.DetectableObjectConfigurationRepository;
 import app.bpartners.geojobs.repository.TileDetectionTaskRepository;
+import app.bpartners.geojobs.repository.model.TileDetectionTask;
 import app.bpartners.geojobs.repository.model.detection.DetectableObjectConfiguration;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ParcelDetectionJobCreatedService implements Consumer<ParcelDetectionJobCreated> {
   private final TileDetectionTaskRepository tileDetectionTaskRepository;
   private final DetectableObjectConfigurationRepository objectConfigurationRepository;
@@ -25,6 +28,7 @@ public class ParcelDetectionJobCreatedService implements Consumer<ParcelDetectio
     var zdjId = parcelDetectionJobCreated.getZdjId();
     var jobId = parcelDetectionJob.getId();
     var tileDetectionTasks = tileDetectionTaskRepository.findAllByJobId(jobId);
+    log.info("[DEBUG] TileDetectionTasks size={} and content ={}", tileDetectionTasks.size(), tileDetectionTasks.stream().map(TileDetectionTask::describeTile));
     var detectableTypes =
         objectConfigurationRepository.findAllByDetectionJobId(zdjId).stream()
             .map(DetectableObjectConfiguration::getObjectType)
