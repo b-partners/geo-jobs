0140120f6a61e6ace0d94b8803532e8eddfda448
Ryan Andriamahery <ryan@hei.school>
2024-06-25 09:27:24 +0300

    chore: JobAnnotationProcessedServiceTest

---
+++ src/test/java/app/bpartners/geojobs/service/event/JobAnnotationProcessedServiceTest.java
@@ -0,0 +1,33 @@
+package app.bpartners.geojobs.service.event;
+
+import static org.mockito.Mockito.*;
+
+import app.bpartners.geojobs.endpoint.event.model.JobAnnotationProcessed;
+import org.junit.jupiter.api.Test;
+
+public class JobAnnotationProcessedServiceTest {
+  ZoneDetectionJobAnnotationProcessor jobAnnotationProcessorMock = mock();
+  JobAnnotationProcessedService subject =
+      new JobAnnotationProcessedService(jobAnnotationProcessorMock);
+
+  @Test
+  void accept_ok() {
+    String jobId = "jobId";
+    double minConfidence = 0.8;
+    String truePositive = "truePositive";
+    String falsePositive = "falsePositive";
+    String withoutObjects = "withoutObjects";
+
+    subject.accept(
+        JobAnnotationProcessed.builder()
+            .jobId(jobId)
+            .minConfidence(minConfidence)
+            .annotationJobWithObjectsIdTruePositive(truePositive)
+            .annotationJobWithObjectsIdFalsePositive(falsePositive)
+            .annotationJobWithoutObjectsId(withoutObjects)
+            .build());
+
+    verify(jobAnnotationProcessorMock, times(1))
+        .accept(jobId, minConfidence, truePositive, falsePositive, withoutObjects);
+  }
+}
