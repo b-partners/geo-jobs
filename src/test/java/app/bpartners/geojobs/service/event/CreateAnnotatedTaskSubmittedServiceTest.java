2ab2691f7a3a8ce2690fd0ea71f883339570208f
Ryan Andriamahery <ryan@hei.school>
2024-06-27 12:57:05 +0300

    chore: CreateAnnotatedTaskSubmittedServiceTest

---
+++ src/test/java/app/bpartners/geojobs/service/event/CreateAnnotatedTaskSubmittedServiceTest.java
@@ -0,0 +1,51 @@
+package app.bpartners.geojobs.service.event;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+import app.bpartners.gen.annotator.endpoint.rest.model.CreateAnnotatedTask;
+import app.bpartners.geojobs.endpoint.event.model.CreateAnnotatedTaskSubmitted;
+import app.bpartners.geojobs.model.exception.ApiException;
+import app.bpartners.geojobs.service.annotator.AnnotationService;
+import org.junit.jupiter.api.Test;
+
+public class CreateAnnotatedTaskSubmittedServiceTest {
+  AnnotationService annotationServiceMock = mock();
+  CreateAnnotatedTaskSubmittedService subject =
+      new CreateAnnotatedTaskSubmittedService(annotationServiceMock);
+
+  @Test
+  void accept_ok() {
+    String annotationJobId = "annotationJobId";
+    CreateAnnotatedTask createAnnotatedTask = new CreateAnnotatedTask();
+
+    assertDoesNotThrow(
+        () ->
+            subject.accept(
+                CreateAnnotatedTaskSubmitted.builder()
+                    .annotationJobId(annotationJobId)
+                    .createAnnotatedTask(createAnnotatedTask)
+                    .build()));
+
+    verify(annotationServiceMock, times(1)).addAnnotationTask(annotationJobId, createAnnotatedTask);
+  }
+
+  @Test
+  void accept_ko() {
+    String annotationJobId = "annotationJobId";
+    CreateAnnotatedTask createAnnotatedTask = new CreateAnnotatedTask();
+    doThrow(ApiException.class)
+        .when(annotationServiceMock)
+        .addAnnotationTask(annotationJobId, createAnnotatedTask);
+
+    assertThrows(
+        ApiException.class,
+        () ->
+            subject.accept(
+                CreateAnnotatedTaskSubmitted.builder()
+                    .annotationJobId(annotationJobId)
+                    .createAnnotatedTask(createAnnotatedTask)
+                    .build()));
+  }
+}
