179b7125d04dfc547b4e315a5aae2afb8d34559e
Dinasoa <hei.dinasoa@gmail.com>
2024-07-17 10:45:03 +0200

    chore: compute ZTJ and ZDJ statistics to return

--- src/main/java/app/bpartners/geojobs/service/ZoneService.java
+++ src/main/java/app/bpartners/geojobs/service/ZoneService.java
@@ -11,6 +11,7 @@ import app.bpartners.geojobs.endpoint.event.EventProducer;
 import app.bpartners.geojobs.endpoint.event.model.ZDJStatusRecomputingSubmitted;
 import app.bpartners.geojobs.endpoint.event.model.ZTJStatusRecomputingSubmitted;
 import app.bpartners.geojobs.endpoint.rest.controller.mapper.DetectableObjectConfigurationMapper;
+import app.bpartners.geojobs.endpoint.rest.controller.mapper.TaskStatisticMapper;
 import app.bpartners.geojobs.endpoint.rest.controller.mapper.ZoneTilingJobMapper;
 import app.bpartners.geojobs.endpoint.rest.model.CreateFullDetection;
 import app.bpartners.geojobs.endpoint.rest.model.DetectableObjectConfiguration;
@@ -20,12 +21,15 @@ import app.bpartners.geojobs.endpoint.rest.security.authorizer.CommunityZoneDete
 import app.bpartners.geojobs.endpoint.rest.security.authorizer.CommunityZoneTilingJobProcessAuthorizer;
 import app.bpartners.geojobs.endpoint.rest.validator.ZoneDetectionJobValidator;
 import app.bpartners.geojobs.job.model.JobStatus;
+import app.bpartners.geojobs.job.model.statistic.TaskStatistic;
 import app.bpartners.geojobs.model.exception.ApiException;
 import app.bpartners.geojobs.repository.model.detection.ZoneDetectionJob;
 import app.bpartners.geojobs.repository.model.tiling.ZoneTilingJob;
 import app.bpartners.geojobs.service.detection.ZoneDetectionJobService;
 import app.bpartners.geojobs.service.tiling.ZoneTilingJobService;
 import java.util.List;
+import java.util.stream.Stream;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -38,9 +42,10 @@ public class ZoneService {
   private final CommunityZoneDetectionJobProcessAuthorizer
       communityZoneDetectionJobProcessAuthorizer;
   private final CommunityZoneTilingJobProcessAuthorizer communityZoneTilingJobProcessAuthorizer;
-  private final ZoneDetectionJobValidator zoneDetectionJobValidator;
+  private final ZoneDetectionJobValidator jobValidator;
   private final DetectableObjectConfigurationMapper objectConfigurationMapper;
   private final EventProducer eventProducer;
+  private final TaskStatisticMapper taskStatisticMapper;
 
   public DetectedZone processTilingAndDetection(CreateFullDetection zoneToDetect) {
     var createJob = zoneTilingJobMapper.from(zoneToDetect);
@@ -48,11 +53,11 @@ public class ZoneService {
     var job = zoneTilingJobMapper.toDomain(createJob);
     var tilingTasks = getTilingTasks(createJob, job.getId());
     ZoneTilingJob zoneTilingJob = zoneTilingJobService.create(job, tilingTasks);
-    String jobId = zoneTilingJob.getId();
-    zoneDetectionJobValidator.accept(zoneTilingJob.getId());
+    String ZTJId = zoneTilingJob.getId();
+    jobValidator.accept(zoneTilingJob.getId());
     JobStatus ZTJStatus = zoneTilingJob.getStatus();
     if (!FINISHED.equals(ZTJStatus.getProgression())) {
-      eventProducer.accept(List.of(new ZTJStatusRecomputingSubmitted(jobId)));
+      eventProducer.accept(List.of(new ZTJStatusRecomputingSubmitted(ZTJId)));
     }
 
     DetectableObjectType detectableObjectType = zoneToDetect.getObjectType();
@@ -61,20 +66,25 @@ public class ZoneService {
     }
     List<DetectableObjectConfiguration> detectableObjectConfigurations = List.of
             (new DetectableObjectConfiguration().type(detectableObjectType));
-    communityZoneDetectionJobProcessAuthorizer.accept(jobId, detectableObjectConfigurations);
+    communityZoneDetectionJobProcessAuthorizer.accept(ZTJId, detectableObjectConfigurations);
     List<app.bpartners.geojobs.repository.model.detection.DetectableObjectConfiguration>
         configurations =
             detectableObjectConfigurations.stream()
-                .map(objectConf -> objectConfigurationMapper.toDomain(jobId, objectConf))
+                .map(objectConf -> objectConfigurationMapper.toDomain(ZTJId, objectConf))
                 .toList();
-    ZoneDetectionJob processedZDJ = zoneDetectionJobService.fireTasks(jobId, configurations);
+    ZoneDetectionJob processedZDJ = zoneDetectionJobService.fireTasks(ZTJId, configurations);
     JobStatus jobStatus = processedZDJ.getStatus();
+    String ZDJId = processedZDJ.getId();
     if (!jobStatus.getProgression().equals(FINISHED)) {
       eventProducer.accept(List.of(new ZDJStatusRecomputingSubmitted(processedZDJ.getId())));
     }
 
+    TaskStatistic ZTJStat = zoneTilingJobService.computeTaskStatistics(ZTJId);
+    TaskStatistic ZDJStat = zoneDetectionJobService.computeTaskStatistics(ZDJId);
+
     return new DetectedZone()
         .jobTypes(List.of(TILING, MACHINE_DETECTION, HUMAN_DETECTION))
-        .detectedGeojsonUrl(zoneDetectionJobService.getGeoJsonsUrl(processedZDJ.getId()).toString());
+        .detectedGeojsonUrl(zoneDetectionJobService.getGeoJsonsUrl(processedZDJ.getId()).toString())
+        .statistics(Stream.of(ZTJStat, ZDJStat).map(taskStatisticMapper::toRest).toList());
   }
 }
