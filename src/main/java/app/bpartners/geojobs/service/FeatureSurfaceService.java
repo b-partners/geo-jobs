1dd60a2b70ee443f18dc3d3e59fe41ab95e5f419
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-07-24 11:22:01 +0300

    chore: feature surface calculations
    * chore: feature surface calculations
    
    * test(refactor): refactor FeatureSurfaceServiceIT

---
+++ src/main/java/app/bpartners/geojobs/service/FeatureSurfaceService.java
@@ -0,0 +1,24 @@
+package app.bpartners.geojobs.service;
+
+import app.bpartners.geojobs.endpoint.rest.controller.mapper.FeatureMapper;
+import app.bpartners.geojobs.endpoint.rest.model.Feature;
+import app.bpartners.geojobs.model.parcelization.area.AreaComputer;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FeatureSurfaceService {
+  private static final AreaComputer AREA_COMPUTER = new AreaComputer();
+  private final FeatureMapper featureMapper;
+
+  double getAreaValue(Feature feature) {
+    var areaValue = AREA_COMPUTER.apply(featureMapper.toDomain(feature));
+    return areaValue.getValue();
+  }
+
+  double getAreaValue(List<Feature> features) {
+    return features.stream().mapToDouble(this::getAreaValue).sum();
+  }
+}
