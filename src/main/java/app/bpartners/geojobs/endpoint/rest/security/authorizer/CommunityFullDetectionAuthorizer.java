95dd7aa3cc120c19f70c7a3db4a88b3bfd6e9557
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-08-01 08:49:22 +0300

    chore: fulllDetection authorization
    * chore: remove current community authorization
    
    * chore: add fullDetection authorizations
    
    * test: features mock
    
    * feat: surface and object type authorizer
    
    * fix: change community repository
    
    * test: basic authorizer test
    
    * test: add empty zone test
    
    * refactor: use biconsumer for each authorizer

---
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/security/authorizer/CommunityFullDetectionAuthorizer.java
@@ -0,0 +1,35 @@
+package app.bpartners.geojobs.endpoint.rest.security.authorizer;
+
+import app.bpartners.geojobs.endpoint.rest.model.CreateFullDetection;
+import app.bpartners.geojobs.endpoint.rest.security.AuthProvider;
+import app.bpartners.geojobs.model.exception.ForbiddenException;
+import app.bpartners.geojobs.repository.CommunityAuthorizationRepository;
+import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommunityFullDetectionAuthorizer implements Consumer<CreateFullDetection> {
+  private final CommunityDetectableObjectTypeAuthorizer communityDetectableObjectTypeAuthorizer;
+  private final CommunityAuthorizationRepository caRepository;
+  private final CommunityZoneAuthorizer communityZoneAuthorizer;
+  private final CommunityZoneSurfaceAuthorizer communityZoneSurfaceAuthorizer;
+
+  @Override
+  public void accept(CreateFullDetection createFullDetection) {
+    var userPrincipal = AuthProvider.getPrincipal();
+    if (userPrincipal.isAdmin()) return;
+
+    var communityAuthorization =
+        caRepository.findByApiKey(userPrincipal.getPassword()).orElseThrow(ForbiddenException::new);
+
+    communityDetectableObjectTypeAuthorizer.accept(
+        communityAuthorization, createFullDetection.getObjectType());
+    if (createFullDetection.getFeatures() != null) {
+      communityZoneSurfaceAuthorizer.accept(
+          communityAuthorization, createFullDetection.getFeatures());
+      communityZoneAuthorizer.accept(communityAuthorization, createFullDetection.getFeatures());
+    }
+  }
+}
---
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/security/authorizer/CommunityFullDetectionAuthorizerTest.java
@@ -0,0 +1,75 @@
+package app.bpartners.geojobs.endpoint.rest.security.authorizer;
+
+import static app.bpartners.geojobs.endpoint.rest.security.model.Authority.Role.ROLE_ADMIN;
+import static app.bpartners.geojobs.endpoint.rest.security.model.Authority.Role.ROLE_COMMUNITY;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import app.bpartners.geojobs.endpoint.rest.model.CreateFullDetection;
+import app.bpartners.geojobs.endpoint.rest.security.AuthProvider;
+import app.bpartners.geojobs.endpoint.rest.security.model.Authority;
+import app.bpartners.geojobs.endpoint.rest.security.model.Principal;
+import app.bpartners.geojobs.repository.CommunityAuthorizationRepository;
+import app.bpartners.geojobs.repository.model.community.CommunityAuthorization;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+class CommunityFullDetectionAuthorizerTest {
+  CommunityAuthorization communityAuthorization = mock();
+  CreateFullDetection createFullDetection = mock();
+  CommunityZoneSurfaceAuthorizer communityZoneSurfaceAuthorizer = mock();
+  CommunityZoneAuthorizer communityZoneAuthorizer = mock();
+  CommunityDetectableObjectTypeAuthorizer communityDetectableObjectTypeAuthorizer = mock();
+  CommunityAuthorizationRepository caRepository = mock();
+  CommunityFullDetectionAuthorizer subject =
+      new CommunityFullDetectionAuthorizer(
+          communityDetectableObjectTypeAuthorizer,
+          caRepository,
+          communityZoneAuthorizer,
+          communityZoneSurfaceAuthorizer);
+  private final MockedStatic<AuthProvider> authProvider = mockStatic(AuthProvider.class);
+
+  @BeforeEach
+  void setup() {
+    when(caRepository.findByApiKey(any())).thenReturn(Optional.of(communityAuthorization));
+  }
+
+  @AfterEach
+  void cleanMock() {
+    authProvider.close();
+  }
+
+  @Test
+  void should_accept_directly_admin_api_key() {
+    useRole(ROLE_ADMIN);
+    when(communityAuthorization.getAuthorizedZones()).thenReturn(List.of());
+
+    assertDoesNotThrow(
+        () -> {
+          subject.accept(createFullDetection);
+        });
+  }
+
+  @Test
+  void should_accept_community_if_authorization_is_correct() {
+    useRole(ROLE_COMMUNITY);
+    doNothing().when(communityZoneSurfaceAuthorizer).accept(any(), any());
+    doNothing().when(communityZoneAuthorizer).accept(any(), any());
+    doNothing().when(communityDetectableObjectTypeAuthorizer).accept(any(), any());
+
+    assertDoesNotThrow(
+        () -> {
+          subject.accept(createFullDetection);
+        });
+  }
+
+  private void useRole(Authority.Role role) {
+    var userPrincipal = new Principal("dummyKey", Set.of(new Authority(role)));
+    authProvider.when(AuthProvider::getPrincipal).thenReturn(userPrincipal);
+  }
+}
