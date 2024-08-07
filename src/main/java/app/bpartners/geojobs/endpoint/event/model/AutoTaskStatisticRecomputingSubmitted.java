5fddea6fdb84e1117ded982b04bcbef20cdbc42d
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-08-07 11:18:48 +0300

    chore: refactor task statictic recomputing

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/AutoTaskStatisticRecomputingSubmitted.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/AutoTaskStatisticRecomputingSubmitted.java
@@ -2,36 +2,21 @@ package app.bpartners.geojobs.endpoint.event.model;
 
 import app.bpartners.geojobs.endpoint.event.EventStack;
 import java.time.Duration;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Getter
 @EqualsAndHashCode(callSuper = false)
 @ToString
-public class AutoTaskStatisticRecomputingSubmitted extends PojaEvent {
+public class AutoTaskStatisticRecomputingSubmitted extends StatisticRecomputingSubmitted {
   private static final long MAX_CONSUMER_DURATION_VALUE = 300L;
   private static final long DEFAULT_BACK_OFF_VALUE = 180L;
-  private String jobId;
-  private long maxConsumerDurationValue;
-  private long maxConsumerBackoffBetweenRetriesDurationValue;
-  private int attemptNb;
 
   public AutoTaskStatisticRecomputingSubmitted(String jobId) {
-    this.jobId = jobId;
-    this.maxConsumerDurationValue = MAX_CONSUMER_DURATION_VALUE;
-    this.maxConsumerBackoffBetweenRetriesDurationValue = DEFAULT_BACK_OFF_VALUE;
-    this.attemptNb = 0;
-  }
-
-  public AutoTaskStatisticRecomputingSubmitted(
-      String jobId, long maxConsumerBackoffBetweenRetriesDurationValue, int attemptNb) {
-    this.jobId = jobId;
-    this.maxConsumerDurationValue = MAX_CONSUMER_DURATION_VALUE;
-    this.maxConsumerBackoffBetweenRetriesDurationValue =
-        maxConsumerBackoffBetweenRetriesDurationValue;
-    this.attemptNb = attemptNb;
+    super(jobId, MAX_CONSUMER_DURATION_VALUE, DEFAULT_BACK_OFF_VALUE);
   }
 
   @Override
--- src/main/java/app/bpartners/geojobs/service/event/AutoTaskStatisticRecomputingSubmittedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/AutoTaskStatisticRecomputingSubmittedService.java
@@ -1,6 +1,5 @@
 package app.bpartners.geojobs.service.event;
 
-import app.bpartners.geojobs.endpoint.event.EventProducer;
 import app.bpartners.geojobs.endpoint.event.model.AutoTaskStatisticRecomputingSubmitted;
 import app.bpartners.geojobs.endpoint.event.model.TaskStatisticRecomputingSubmitted;
 import app.bpartners.geojobs.job.model.Job;
@@ -10,7 +9,6 @@ import app.bpartners.geojobs.repository.model.detection.ParcelDetectionTask;
 import app.bpartners.geojobs.repository.model.detection.ZoneDetectionJob;
 import app.bpartners.geojobs.repository.model.tiling.TilingTask;
 import app.bpartners.geojobs.repository.model.tiling.ZoneTilingJob;
-import java.util.List;
 import java.util.function.Consumer;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +16,6 @@ import org.springframework.stereotype.Service;
 public class AutoTaskStatisticRecomputingSubmittedService
     implements Consumer<AutoTaskStatisticRecomputingSubmitted> {
   private static final int ATTEMPT_FOR_256_MINUTES_DURATION = 6;
-  private final EventProducer eventProducer;
   private final TaskStatisticRecomputingSubmittedConsumer<TilingTask, ZoneTilingJob>
       tilingStatisticConsumer;
   private final TaskStatisticRecomputingSubmittedConsumer<ParcelDetectionTask, ZoneDetectionJob>
@@ -27,13 +24,11 @@ public class AutoTaskStatisticRecomputingSubmittedService
   private final ZoneDetectionJobRepository zoneDetectionJobRepository;
 
   public AutoTaskStatisticRecomputingSubmittedService(
-      EventProducer eventProducer,
       ZoneDetectionJobRepository zoneDetectionJobRepository,
       ParcelDetectionTaskRepository parcelDetectionTaskRepository,
       ZoneTilingJobRepository zoneTilingJobRepository,
       TilingTaskRepository tilingTaskRepository,
       TaskStatisticRepository taskStatisticRepository) {
-    this.eventProducer = eventProducer;
     this.tilingJobRepository = zoneTilingJobRepository;
     this.zoneDetectionJobRepository = zoneDetectionJobRepository;
     this.zoneDetectionStatisticConsumer =
@@ -47,7 +42,6 @@ public class AutoTaskStatisticRecomputingSubmittedService
   @Override
   public void accept(AutoTaskStatisticRecomputingSubmitted event) {
     var jobId = event.getJobId();
-    var lastDurationValue = event.getMaxConsumerBackoffBetweenRetriesDurationValue();
     var attemptNb = event.getAttemptNb();
     var optionalTiling = tilingJobRepository.findById(jobId);
     if (optionalTiling.isEmpty()) {
@@ -55,30 +49,26 @@ public class AutoTaskStatisticRecomputingSubmittedService
           zoneDetectionJobRepository
               .findById(jobId)
               .orElseThrow(() -> new NotFoundException("Job.id=" + jobId + " not found"));
-      processJob(zoneDetectionJob, jobId, lastDurationValue, attemptNb);
+      processJob(zoneDetectionJob, jobId, attemptNb);
     } else {
       var tilingJob = optionalTiling.get();
-      processJob(tilingJob, jobId, lastDurationValue, attemptNb);
+      processJob(tilingJob, jobId, attemptNb);
     }
   }
 
-  private void processJob(Job job, String jobId, long lastDurationValue, int attemptNb) {
+  private void processJob(Job job, String jobId, int attemptNb) {
     var taskStatisticRecomputingSubmitted = new TaskStatisticRecomputingSubmitted(jobId);
     if (job instanceof ZoneDetectionJob) {
       zoneDetectionStatisticConsumer.accept(taskStatisticRecomputingSubmitted);
     } else if (job instanceof ZoneTilingJob) {
       tilingStatisticConsumer.accept(taskStatisticRecomputingSubmitted);
     }
-
-    if (!job.isFailed() && !job.isSucceeded()) {
-      if (attemptNb < ATTEMPT_FOR_256_MINUTES_DURATION) {
-        long maxConsumerBackoffBetweenRetriesDurationValue = lastDurationValue * 2;
-        int newAttemptNb = attemptNb + 1;
-        eventProducer.accept(
-            List.of(
-                new AutoTaskStatisticRecomputingSubmitted(
-                    jobId, maxConsumerBackoffBetweenRetriesDurationValue, newAttemptNb)));
-      }
+    if (job.isFinished()) {
+      return;
     }
+    if (attemptNb > ATTEMPT_FOR_256_MINUTES_DURATION) {
+      return;
+    }
+    throw new RuntimeException("Fail on purpose so that message is not ack, causing retry");
   }
 }
