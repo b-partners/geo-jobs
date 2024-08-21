ebd0b106f243135e61e2cdbe67c2607020012b71
Ryan Andriamahery <ryan@hei.school>
2024-08-21 13:45:17 +0300

    chore: fix TileDetectionTaskCreatedServiceTest and ZDJStatusRecomputingSubmittedServiceTest

--- src/test/java/app/bpartners/geojobs/service/event/TileDetectionTaskCreatedServiceTest.java
+++ src/test/java/app/bpartners/geojobs/service/event/TileDetectionTaskCreatedServiceTest.java
@@ -4,14 +4,12 @@ import static app.bpartners.geojobs.job.model.Status.HealthStatus.SUCCEEDED;
 import static app.bpartners.geojobs.job.model.Status.HealthStatus.UNKNOWN;
 import static app.bpartners.geojobs.job.model.Status.ProgressionStatus.*;
 import static app.bpartners.geojobs.repository.model.detection.DetectableType.PATHWAY;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import app.bpartners.geojobs.endpoint.event.EventProducer;
 import app.bpartners.geojobs.endpoint.event.model.tile.TileDetectionTaskCreated;
-import app.bpartners.geojobs.endpoint.event.model.tile.TileDetectionTaskCreatedFailed;
 import app.bpartners.geojobs.endpoint.event.model.tile.TileDetectionTaskSucceeded;
 import app.bpartners.geojobs.job.model.TaskStatus;
 import app.bpartners.geojobs.model.exception.ApiException;
@@ -22,7 +20,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-public class TileDetectionTaskCreatedServiceTest {
+class TileDetectionTaskCreatedServiceTest {
   TileDetectionTaskStatusService tileDetectionTaskStatusServiceMock = mock();
   TileDetectionTaskCreatedConsumer tileDetectionTaskConsumerMock = mock();
   EventProducer eventProducerMock = mock();
@@ -89,13 +87,10 @@ public class TileDetectionTaskCreatedServiceTest {
             "zdjId",
             TileDetectionTask.builder().build(),
             List.of(DetectableObjectConfiguration.builder().objectType(PATHWAY).build()));
-    assertDoesNotThrow(() -> subject.accept(expectedTileDetectionTaskCreated));
+
+    assertThrows(ApiException.class, () -> subject.accept(expectedTileDetectionTaskCreated));
+
     var eventCaptor = ArgumentCaptor.forClass(List.class);
-    verify(eventProducerMock, times(1)).accept(eventCaptor.capture());
-    TileDetectionTaskCreatedFailed capturedTileDetectionTaskCreatedFailed =
-        (TileDetectionTaskCreatedFailed) eventCaptor.getValue().getFirst();
-    TileDetectionTaskCreated capturedTileDetectionTaskCreated =
-        capturedTileDetectionTaskCreatedFailed.getTileDetectionTaskCreated();
-    assertEquals(expectedTileDetectionTaskCreated, capturedTileDetectionTaskCreated);
+    verify(eventProducerMock, times(0)).accept(eventCaptor.capture());
   }
 }
