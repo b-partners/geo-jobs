1c98e8ee9e54cac98aa07222c9e91e7520840a51
Lou Andria <lou@hei.school>
2024-09-03 11:53:23 +0200

    fix: CreateFullDetection with no e2e id ends in BAD_REQUEST and has priority over any other validation

--- src/main/java/app/bpartners/geojobs/endpoint/rest/validator/CreateFullDetectionValidator.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/validator/CreateFullDetectionValidator.java
@@ -1,10 +1,8 @@
 package app.bpartners.geojobs.endpoint.rest.validator;
 
-import static app.bpartners.geojobs.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
-
 import app.bpartners.geojobs.endpoint.rest.model.CreateFullDetection;
 import app.bpartners.geojobs.endpoint.rest.security.authorizer.CommunityFullDetectionAuthorizer;
-import app.bpartners.geojobs.model.exception.ApiException;
+import app.bpartners.geojobs.model.exception.BadRequestException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +12,10 @@ public class CreateFullDetectionValidator {
   private CommunityFullDetectionAuthorizer communityFullDetectionAuthorizer;
 
   public void accept(CreateFullDetection createFullDetection) {
-    communityFullDetectionAuthorizer.accept(createFullDetection);
     if (createFullDetection.getEndToEndId() == null) {
-      throw new ApiException(
-          SERVER_EXCEPTION,
-          "You are not allowed to perform the full detection, end to end id was not provided");
+      throw new BadRequestException("You must provide an end-to-end id for your detection");
     }
+
+    communityFullDetectionAuthorizer.accept(createFullDetection);
   }
 }
