067559284c10a2fb17f1a1c29c31a8f254c07a90
Ryan Andriamahery <ryan@hei.school>
2024-09-17 15:37:07 +0300

    chore: notify admin by email after annotation job delivered

--- src/main/java/app/bpartners/geojobs/service/event/AnnotationDeliveryJobStatusChangedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/AnnotationDeliveryJobStatusChangedService.java
@@ -9,13 +9,20 @@ import static java.util.UUID.randomUUID;
 
 import app.bpartners.geojobs.endpoint.event.model.annotation.AnnotationDeliveryJobStatusChanged;
 import app.bpartners.geojobs.job.model.JobStatus;
+import app.bpartners.geojobs.mail.Email;
+import app.bpartners.geojobs.mail.Mailer;
 import app.bpartners.geojobs.repository.model.annotation.AnnotationDeliveryJob;
 import app.bpartners.geojobs.service.StatusChangedHandler;
 import app.bpartners.geojobs.service.StatusHandler;
 import app.bpartners.geojobs.service.annotator.AnnotationService;
 import app.bpartners.geojobs.service.detection.ZoneDetectionJobService;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import java.io.File;
+import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +34,7 @@ public class AnnotationDeliveryJobStatusChangedService
   private final StatusChangedHandler statusChangedHandler;
   private final AnnotationService annotationService;
   private final ZoneDetectionJobService zoneDetectionJobService;
+  private final Mailer mailer;
 
   @Override
   public void accept(AnnotationDeliveryJobStatusChanged event) {
@@ -34,7 +42,7 @@ public class AnnotationDeliveryJobStatusChangedService
     var newJob = event.getNewJob();
 
     OnFinishedHandler onFinishedHandler =
-        new OnFinishedHandler(zoneDetectionJobService, annotationService, newJob);
+        new OnFinishedHandler(zoneDetectionJobService, annotationService, mailer, newJob);
 
     statusChangedHandler.handle(
         event, newJob.getStatus(), oldJob.getStatus(), onFinishedHandler, onFinishedHandler);
@@ -43,9 +51,11 @@ public class AnnotationDeliveryJobStatusChangedService
   private record OnFinishedHandler(
       ZoneDetectionJobService zoneDetectionJobService,
       AnnotationService annotationService,
+      Mailer mailer,
       AnnotationDeliveryJob newJob)
       implements StatusHandler {
 
+    @SneakyThrows
     @Override
     public String performAction() {
       var detectionJobId = newJob.getDetectionJobId();
@@ -55,6 +65,32 @@ public class AnnotationDeliveryJobStatusChangedService
 
       annotationService.saveAnnotationJob(
           detectionJobId, annotationJobId, annotationJobName, labels, STARTED);
+      updateZDJStatus(detectionJobId);
+      notifyAdminByEmail(newJob);
+
+      return "Annotation Delivery Job (id"
+          + newJob.getId()
+          + ") finished with status "
+          + newJob.getStatus();
+    }
+
+    private void notifyAdminByEmail(AnnotationDeliveryJob deliveryJob) throws AddressException {
+      List<InternetAddress> cc = List.of();
+      List<InternetAddress> bcc = List.of();
+      String subject =
+          "AnnotationJob(id="
+              + deliveryJob.getAnnotationJobId()
+              + ",nom="
+              + deliveryJob.getAnnotationJobName()
+              + ") disponible sur annotator";
+      String htmlBody = "";
+      List<File> attachments = List.of();
+      mailer.accept(
+          new Email(
+              new InternetAddress("tech@bpartners.app"), cc, bcc, subject, htmlBody, attachments));
+    }
+
+    private void updateZDJStatus(String detectionJobId) {
       var humanZDJ = zoneDetectionJobService.getHumanZdjFromZdjId(detectionJobId);
       humanZDJ.hasNewStatus(
           JobStatus.builder()
@@ -66,11 +102,6 @@ public class AnnotationDeliveryJobStatusChangedService
               .creationDatetime(now())
               .build());
       zoneDetectionJobService.save(humanZDJ);
-
-      return "Annotation Delivery Job (id"
-          + newJob.getId()
-          + ") finished with status "
-          + newJob.getStatus();
     }
   }
 }
