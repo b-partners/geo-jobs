4c8f740508ca01b98154f3e360dc492f9a9f2c1a
RickaPrincy <rckprincy@gmail.com>
2024-09-23 09:03:46 +0300

    chore: add fullDetections in security conf

--- src/main/java/app/bpartners/geojobs/endpoint/rest/security/SecurityConf.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/security/SecurityConf.java
@@ -108,6 +108,8 @@ public class SecurityConf {
                     .hasAuthority(ROLE_ADMIN.name())
                     .requestMatchers(PUT, "/fullDetection")
                     .hasAnyAuthority(ROLE_ADMIN.name(), ROLE_COMMUNITY.name())
+                    .requestMatchers(GET, "/fullDetections")
+                    .hasAnyAuthority(ROLE_ADMIN.name(), ROLE_COMMUNITY.name())
                     .requestMatchers(GET, "/usage")
                     .hasAnyAuthority(ROLE_ADMIN.name(), ROLE_COMMUNITY.name())
                     .anyRequest()
