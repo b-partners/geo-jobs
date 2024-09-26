e53d47569580398ff8f8ce86445dcee2f7159b7e
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2024-09-26 13:00:38 +0300

    feat: add new BP detectable models
    * docs: add BP_ZAN model
    
    * chore: add BP_ZAN model
    
    * chore: add detectable object type
    BP_CLIMAT_RESILIENCE
    
    * chore: add detectable object type
    BP_CONFORMITE_PLU
    
    * chore: add detectable object type
    BP_CONFORMITE_PLU
    
    * chore: add detectable object type
    BP_TROTTOIRS
    
    * chore: add detectable object type
    BP_OLD

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/DetectableObjectTypeMapper.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/DetectableObjectTypeMapper.java
@@ -4,9 +4,7 @@ import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.*;
 import static app.bpartners.geojobs.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
 import static java.util.UUID.randomUUID;
 
-import app.bpartners.geojobs.endpoint.rest.model.BPLomModel;
-import app.bpartners.geojobs.endpoint.rest.model.BPToitureModel;
-import app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType;
+import app.bpartners.geojobs.endpoint.rest.model.*;
 import app.bpartners.geojobs.model.exception.ApiException;
 import app.bpartners.geojobs.model.exception.NotImplementedException;
 import app.bpartners.geojobs.repository.model.detection.DetectableObjectConfiguration;
@@ -22,83 +20,156 @@ public class DetectableObjectTypeMapper {
   public static final double DEFAULT_CONFIDENCE = 1.0;
 
   public DetectableType toDomain(DetectableObjectType rest) {
-    switch (rest) {
-      case PISCINE -> {
-        return DetectableType.PISCINE;
-      }
-      case TOITURE_REVETEMENT -> {
-        return DetectableType.TOITURE_REVETEMENT;
-      }
-      case ARBRE -> {
-        return DetectableType.ARBRE;
-      }
-      case PASSAGE_PIETON -> {
-        return DetectableType.PASSAGE_PIETON;
-      }
-      case PANNEAU_PHOTOVOLTAIQUE -> {
-        return DetectableType.PANNEAU_PHOTOVOLTAIQUE;
-      }
+    return switch (rest) {
+      case PISCINE -> DetectableType.PISCINE;
+      case TOITURE_REVETEMENT -> DetectableType.TOITURE_REVETEMENT;
+      case ARBRE -> DetectableType.ARBRE;
+      case PASSAGE_PIETON -> DetectableType.PASSAGE_PIETON;
+      case PANNEAU_PHOTOVOLTAIQUE -> DetectableType.PANNEAU_PHOTOVOLTAIQUE;
+      case TROTTOIR -> DetectableType.TROTTOIR;
+      case LINE -> DetectableType.LINE;
+      case ESPACE_VERT -> DetectableType.ESPACE_VERT;
+      case VOIE_CARROSSABLE -> DetectableType.VOIE_CARROSSABLE;
+      case MOISISSURE -> DetectableType.MOISISSURE;
+      case USURE -> DetectableType.USURE;
+      case FISSURE_CASSURE -> DetectableType.FISSURE_CASSURE;
+      case OBSTACLE -> DetectableType.OBSTACLE;
+      case CHEMINEE -> DetectableType.CHEMINEE;
+      case HUMIDITE -> DetectableType.HUMIDITE;
+      case RISQUE_FEU -> DetectableType.RISQUE_FEU;
+      case VELUX -> DetectableType.VELUX;
       default -> throw new NotImplementedException("Unknown detectable object type " + rest);
-    }
+    };
   }
 
   public DetectableObjectType toRest(DetectableType domain) {
-    switch (domain) {
-      case PISCINE -> {
-        return PISCINE;
-      }
-      case TOITURE_REVETEMENT -> {
-        return TOITURE_REVETEMENT;
-      }
-      case ARBRE -> {
-        return ARBRE;
-      }
-      case PASSAGE_PIETON -> {
-        return PASSAGE_PIETON;
-      }
-      case PANNEAU_PHOTOVOLTAIQUE -> {
-        return PANNEAU_PHOTOVOLTAIQUE;
-      }
-      case TROTTOIR -> {
-        return TROTTOIR;
-      }
-      case LINE -> {
-        return LINE;
-      }
-      case ESPACE_VERT -> {
-        return ESPACE_VERT;
-      }
+    return switch (domain) {
+      case PISCINE -> DetectableObjectType.PISCINE;
+      case TOITURE_REVETEMENT -> DetectableObjectType.TOITURE_REVETEMENT;
+      case ARBRE -> DetectableObjectType.ARBRE;
+      case PASSAGE_PIETON -> DetectableObjectType.PASSAGE_PIETON;
+      case PANNEAU_PHOTOVOLTAIQUE -> DetectableObjectType.PANNEAU_PHOTOVOLTAIQUE;
+      case TROTTOIR -> DetectableObjectType.TROTTOIR;
+      case LINE -> DetectableObjectType.LINE;
+      case ESPACE_VERT -> DetectableObjectType.ESPACE_VERT;
+      case VOIE_CARROSSABLE -> DetectableObjectType.VOIE_CARROSSABLE;
+      case MOISISSURE -> DetectableObjectType.MOISISSURE;
+      case USURE -> DetectableObjectType.USURE;
+      case FISSURE_CASSURE -> DetectableObjectType.FISSURE_CASSURE;
+      case OBSTACLE -> DetectableObjectType.OBSTACLE;
+      case CHEMINEE -> DetectableObjectType.CHEMINEE;
+      case HUMIDITE -> DetectableObjectType.HUMIDITE;
+      case RISQUE_FEU -> DetectableObjectType.RISQUE_FEU;
+      case VELUX -> DetectableObjectType.VELUX;
       default -> throw new NotImplementedException("Unknown detectable object type " + domain);
-    }
+    };
   }
 
   public List<DetectableObjectType> mapFromModel(Object o) {
-    var objectTypes = new ArrayList<DetectableObjectType>();
-    if (o instanceof BPToitureModel) {
-      BPToitureModel model = (BPToitureModel) o;
-      if (model.getArbre().booleanValue()) {
-        objectTypes.add(ARBRE);
-      }
-      if (model.getToitureRevetement().booleanValue()) {
-        objectTypes.add(TOITURE_REVETEMENT);
-      }
-      if (model.getPanneauPhotovoltaique().booleanValue()) {
-        objectTypes.add(PANNEAU_PHOTOVOLTAIQUE);
-      }
-      // TODO: add other detectableTypes in BPToitureModel not handled yet
-    } else if (o instanceof BPLomModel) {
-      BPLomModel model = (BPLomModel) o;
-      if (model.getPassagePieton().booleanValue()) {
-        objectTypes.add(PASSAGE_PIETON);
-      }
-      if (model.getTrottoir().booleanValue()) {
-        objectTypes.add(TROTTOIR);
-      }
-      // TODO: add other detectableTypes in BPLomModel not handled yet
-    } else {
-      throw new ApiException(SERVER_EXCEPTION, "Unknown instance of object " + o.getClass());
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+
+    switch (o) {
+      case BPToitureModel model ->
+          objectTypes.addAll(detectableObjectTypeFromBPToitureModel(model));
+      case BPLomModel model -> objectTypes.addAll(detectableObjectTypeFromBPLomModel(model));
+      case BPZanModel model -> objectTypes.addAll(detectableObjectTypeFromBPZanModel(model));
+      case BPClimatResilienceModel model ->
+          objectTypes.addAll(detectableObjectTypeFromBPClimatResilienceModel(model));
+      case BPConformitePluModel model ->
+          objectTypes.addAll(detectableObjectTypeFromBPConformitePluModel(model));
+      case BPTrottoirsModel model ->
+          objectTypes.addAll(detectableObjectTypeFromBPTrottoirsModel(model));
+      case BPOldModel model -> objectTypes.addAll(detectableObjectTypeFromBPOldModel(model));
+      default ->
+          throw new ApiException(SERVER_EXCEPTION, "Unknown instance of object " + o.getClass());
     }
+
     return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPToitureModel(BPToitureModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getToitureRevetement(), objectTypes, TOITURE_REVETEMENT);
+    addIfTrue(model.getPanneauPhotovoltaique(), objectTypes, PANNEAU_PHOTOVOLTAIQUE);
+    addIfTrue(model.getMoisissure(), objectTypes, MOISISSURE);
+    addIfTrue(model.getUsure(), objectTypes, USURE);
+    addIfTrue(model.getFissureCassure(), objectTypes, FISSURE_CASSURE);
+    addIfTrue(model.getObstacle(), objectTypes, OBSTACLE);
+    addIfTrue(model.getCheminee(), objectTypes, CHEMINEE);
+    addIfTrue(model.getHumidite(), objectTypes, HUMIDITE);
+    addIfTrue(model.getRisqueFeu(), objectTypes, RISQUE_FEU);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPLomModel(BPLomModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getPassagePieton(), objectTypes, PASSAGE_PIETON);
+    addIfTrue(model.getTrottoir(), objectTypes, TROTTOIR);
+    addIfTrue(model.getVoieCarrosable(), objectTypes, VOIE_CARROSSABLE);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPClimatResilienceModel(
+      BPClimatResilienceModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getParking(), objectTypes, PARKING);
+    addIfTrue(model.getPanneauPhotovoltaique(), objectTypes, PANNEAU_PHOTOVOLTAIQUE);
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getEspaceVert(), objectTypes, ESPACE_VERT);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPZanModel(BPZanModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getEspaceVert(), objectTypes, ESPACE_VERT);
+    addIfTrue(model.getToiture(), objectTypes, TOITURE_REVETEMENT);
+    addIfTrue(model.getVoieCarrossable(), objectTypes, VOIE_CARROSSABLE);
+    addIfTrue(model.getTrottoir(), objectTypes, TROTTOIR);
+    addIfTrue(model.getParking(), objectTypes, PARKING);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPConformitePluModel(
+      BPConformitePluModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getToiture(), objectTypes, TOITURE_REVETEMENT);
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getVelux(), objectTypes, VELUX);
+    addIfTrue(model.getPanneauPhotovoltaique(), objectTypes, PANNEAU_PHOTOVOLTAIQUE);
+    addIfTrue(model.getEspaceVert(), objectTypes, ESPACE_VERT);
+    addIfTrue(model.getPiscine(), objectTypes, PISCINE);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPTrottoirsModel(
+      BPTrottoirsModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getTrottoir(), objectTypes, TROTTOIR);
+    addIfTrue(model.getVoieCarrossable(), objectTypes, VOIE_CARROSSABLE);
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getEspaceVertParking(), objectTypes, ESPACE_VERT_PARKING);
+    return objectTypes;
+  }
+
+  private List<DetectableObjectType> detectableObjectTypeFromBPOldModel(BPOldModel model) {
+    List<DetectableObjectType> objectTypes = new ArrayList<>();
+    addIfTrue(model.getArbre(), objectTypes, ARBRE);
+    addIfTrue(model.getEspaceVert(), objectTypes, ESPACE_VERT);
+    addIfTrue(model.getToiture(), objectTypes, TOITURE_REVETEMENT);
+    addIfTrue(model.getVoieCarrossable(), objectTypes, VOIE_CARROSSABLE);
+    addIfTrue(model.getTrottoir(), objectTypes, TROTTOIR);
+    addIfTrue(model.getParking(), objectTypes, PARKING);
+    addIfTrue(model.getRisqueFeu(), objectTypes, RISQUE_FEU);
+    return objectTypes;
+  }
+
+  private void addIfTrue(
+      Boolean condition, List<DetectableObjectType> objectTypes, DetectableObjectType objectType) {
+    if (Boolean.TRUE.equals(condition)) {
+      objectTypes.add(objectType);
+    }
   }
 
   public List<DetectableObjectConfiguration> mapDefaultConfigurationsFromModel(
--- src/test/java/app/bpartners/geojobs/unit/DetectableObjectTypeMapperTest.java
+++ src/test/java/app/bpartners/geojobs/unit/DetectableObjectTypeMapperTest.java
@@ -1,12 +1,24 @@
 package app.bpartners.geojobs.unit;
 
-import static app.bpartners.geojobs.repository.model.detection.DetectableType.*;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.*;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.ARBRE;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.ESPACE_VERT;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.PANNEAU_PHOTOVOLTAIQUE;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.PARKING;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.PASSAGE_PIETON;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.PISCINE;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.RISQUE_FEU;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.TOITURE_REVETEMENT;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.TROTTOIR;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.VELUX;
+import static app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType.VOIE_CARROSSABLE;
+import static app.bpartners.geojobs.repository.model.detection.DetectableType.LINE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import app.bpartners.geojobs.endpoint.rest.controller.mapper.DetectableObjectTypeMapper;
-import app.bpartners.geojobs.endpoint.rest.model.DetectableObjectType;
-import app.bpartners.geojobs.model.exception.NotImplementedException;
+import app.bpartners.geojobs.endpoint.rest.model.*;
+import app.bpartners.geojobs.repository.model.detection.DetectableType;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class DetectableObjectTypeMapperTest {
@@ -14,33 +26,113 @@ class DetectableObjectTypeMapperTest {
 
   @Test
   void to_domain_ok() {
-    assertEquals(ARBRE, subject.toDomain(DetectableObjectType.ARBRE));
-    assertEquals(TOITURE_REVETEMENT, subject.toDomain(DetectableObjectType.TOITURE_REVETEMENT));
-    assertEquals(PISCINE, subject.toDomain(DetectableObjectType.PISCINE));
-    assertEquals(PASSAGE_PIETON, subject.toDomain(DetectableObjectType.PASSAGE_PIETON));
-    assertEquals(
-        PANNEAU_PHOTOVOLTAIQUE, subject.toDomain(DetectableObjectType.PANNEAU_PHOTOVOLTAIQUE));
-  }
-
-  @Test
-  void to_domain_ko() {
-    assertThrows(
-        NotImplementedException.class, () -> subject.toDomain(DetectableObjectType.TROTTOIR));
-    assertThrows(NotImplementedException.class, () -> subject.toDomain(DetectableObjectType.LINE));
-    assertThrows(
-        NotImplementedException.class, () -> subject.toDomain(DetectableObjectType.ESPACE_VERT));
+    assertEquals(DetectableType.ARBRE, subject.toDomain(ARBRE));
+    assertEquals(DetectableType.TOITURE_REVETEMENT, subject.toDomain(TOITURE_REVETEMENT));
+    assertEquals(DetectableType.PISCINE, subject.toDomain(PISCINE));
+    assertEquals(DetectableType.PASSAGE_PIETON, subject.toDomain(PASSAGE_PIETON));
+    assertEquals(DetectableType.PANNEAU_PHOTOVOLTAIQUE, subject.toDomain(PANNEAU_PHOTOVOLTAIQUE));
   }
 
   @Test
   void to_rest_ok() {
-    assertEquals(DetectableObjectType.ARBRE, subject.toRest(ARBRE));
-    assertEquals(DetectableObjectType.TOITURE_REVETEMENT, subject.toRest(TOITURE_REVETEMENT));
-    assertEquals(DetectableObjectType.PISCINE, subject.toRest(PISCINE));
-    assertEquals(DetectableObjectType.PASSAGE_PIETON, subject.toRest(PASSAGE_PIETON));
-    assertEquals(
-        DetectableObjectType.PANNEAU_PHOTOVOLTAIQUE, subject.toRest(PANNEAU_PHOTOVOLTAIQUE));
+    assertEquals(ARBRE, subject.toRest(DetectableType.ARBRE));
+    assertEquals(TOITURE_REVETEMENT, subject.toRest(DetectableType.TOITURE_REVETEMENT));
+    assertEquals(PISCINE, subject.toRest(DetectableType.PISCINE));
+    assertEquals(PASSAGE_PIETON, subject.toRest(DetectableType.PASSAGE_PIETON));
+    assertEquals(PANNEAU_PHOTOVOLTAIQUE, subject.toRest(DetectableType.PANNEAU_PHOTOVOLTAIQUE));
     assertEquals(DetectableObjectType.LINE, subject.toRest(LINE));
-    assertEquals(DetectableObjectType.TROTTOIR, subject.toRest(TROTTOIR));
-    assertEquals(DetectableObjectType.ESPACE_VERT, subject.toRest(ESPACE_VERT));
+    assertEquals(TROTTOIR, subject.toRest(DetectableType.TROTTOIR));
+    assertEquals(ESPACE_VERT, subject.toRest(DetectableType.ESPACE_VERT));
+  }
+
+  @Test
+  void map_from_model_BP_Toiture_Model() {
+    var object = new BPToitureModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected =
+        List.of(
+            ARBRE,
+            TOITURE_REVETEMENT,
+            PANNEAU_PHOTOVOLTAIQUE,
+            DetectableObjectType.MOISISSURE,
+            DetectableObjectType.USURE,
+            DetectableObjectType.FISSURE_CASSURE,
+            DetectableObjectType.OBSTACLE,
+            DetectableObjectType.CHEMINEE,
+            DetectableObjectType.HUMIDITE,
+            RISQUE_FEU);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_model_BP_Lom_Model() {
+    var object = new BPLomModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected = List.of(PASSAGE_PIETON, TROTTOIR, VOIE_CARROSSABLE);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_model_BP_Zan_Model() {
+    var object = new BPZanModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected =
+        List.of(ARBRE, ESPACE_VERT, TOITURE_REVETEMENT, VOIE_CARROSSABLE, TROTTOIR, PARKING);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_model_BP_Conformite_Plu_Model() {
+    var object = new BPConformitePluModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected =
+        List.of(TOITURE_REVETEMENT, ARBRE, VELUX, PANNEAU_PHOTOVOLTAIQUE, ESPACE_VERT, PISCINE);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_model_BP_Climat_Resilience_Model() {
+    var object = new BPClimatResilienceModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected = List.of(PARKING, PANNEAU_PHOTOVOLTAIQUE, ARBRE, ESPACE_VERT);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_modl_BP_Trottoirs_Model() {
+    var object = new BPTrottoirsModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected = List.of(TROTTOIR, VOIE_CARROSSABLE, ARBRE, ESPACE_VERT_PARKING);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void map_from_modl_BP_Old_Model() {
+    var object = new BPOldModel();
+
+    var actual = subject.mapFromModel(object);
+
+    var expected =
+        List.of(
+            ARBRE,
+            ESPACE_VERT,
+            TOITURE_REVETEMENT,
+            VOIE_CARROSSABLE,
+            TROTTOIR,
+            PARKING,
+            RISQUE_FEU);
+    assertEquals(expected, actual);
   }
 }
