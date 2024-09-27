067b981065dc24027e8d426b5827cf30c0c41da2
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2024-09-27 12:07:09 +0300

    chore: beautify detection saved email template

--- src/main/java/app/bpartners/geojobs/service/event/DetectionSavedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/DetectionSavedService.java
@@ -3,6 +3,7 @@ package app.bpartners.geojobs.service.event;
 import static java.time.Instant.now;
 
 import app.bpartners.geojobs.endpoint.event.model.DetectionSaved;
+import app.bpartners.geojobs.endpoint.rest.model.BPToitureModel;
 import app.bpartners.geojobs.file.bucket.BucketComponent;
 import app.bpartners.geojobs.mail.Email;
 import app.bpartners.geojobs.mail.Mailer;
@@ -10,7 +11,9 @@ import app.bpartners.geojobs.repository.model.detection.Detection;
 import app.bpartners.geojobs.template.HTMLTemplateParser;
 import jakarta.mail.internet.InternetAddress;
 import java.io.File;
+import java.lang.reflect.Field;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
@@ -23,6 +26,8 @@ import org.thymeleaf.context.Context;
 @AllArgsConstructor
 public class DetectionSavedService implements Consumer<DetectionSaved> {
   private static final String DETECTION_SAVED_TEMPLATE = "detection_saved";
+  private static final String BP_MODEL_NAME_ATTRIBUTE_NAME = "modelName";
+  private static final String DEFAULT_MODEL_NAME_KEY = "Nom du modèle";
   private final Mailer mailer;
   private final BucketComponent bucketComponent;
 
@@ -61,11 +66,39 @@ public class DetectionSavedService implements Consumer<DetectionSaved> {
             : bucketComponent
                 .presign(detection.getExcelFileKey(), Duration.ofHours(24L))
                 .toString();
+    var modelActualInstance = detection.getDetectableObjectModel().getActualInstance();
     HTMLTemplateParser htmlTemplateParser = new HTMLTemplateParser();
     Context context = new Context();
+    context.setVariable(
+        "detectableObjectModelStringMapValues", retrieveNameWithBooleanValue(modelActualInstance));
     context.setVariable("detection", detection);
     context.setVariable("shapeFileUrl", shapeFilePresignURL);
     context.setVariable("excelFileUrl", excelFilePresignURL);
+
     return htmlTemplateParser.apply(DETECTION_SAVED_TEMPLATE, context);
   }
+
+  @SneakyThrows
+  private static List<DetectableObjectModelStringMapValue> retrieveNameWithBooleanValue(
+      Object modelInstance) {
+    var detectableObjectStringValues = new ArrayList<DetectableObjectModelStringMapValue>();
+    Field[] fields = modelInstance.getClass().getFields();
+    for (Field field : fields) {
+      field.setAccessible(true);
+      String fieldName = field.getName();
+      if (field.getType().equals(Boolean.class)) {
+        var value = field.getBoolean(modelInstance) ? "oui" : "non";
+        detectableObjectStringValues.add(new DetectableObjectModelStringMapValue(fieldName, value));
+      } else if (BP_MODEL_NAME_ATTRIBUTE_NAME.equals(fieldName)
+          && field.getType().equals(BPToitureModel.ModelNameEnum.class)) {
+        detectableObjectStringValues.add(
+            new DetectableObjectModelStringMapValue(
+                DEFAULT_MODEL_NAME_KEY,
+                ((BPToitureModel.ModelNameEnum) field.get(modelInstance)).getValue()));
+      }
+    }
+    return detectableObjectStringValues;
+  }
+
+  public record DetectableObjectModelStringMapValue(String key, String value) {}
 }
