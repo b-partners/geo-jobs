9a7089f66e8df33bb9d10e6524206bf46c3a8301
Ryan Andriamahery <ryan@hei.school>
2024-11-07 18:10:40 +0300

    chore: handle GeoJsonConversionAssemblyInitiated with EVENT_STACK_2

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/GeoJsonConversionAssemblyInitiated.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/GeoJsonConversionAssemblyInitiated.java
@@ -1,5 +1,8 @@
 package app.bpartners.geojobs.endpoint.event.model;
 
+import static app.bpartners.geojobs.endpoint.event.EventStack.EVENT_STACK_2;
+
+import app.bpartners.geojobs.endpoint.event.EventStack;
 import java.time.Duration;
 import lombok.*;
 
@@ -20,5 +23,10 @@ public class GeoJsonConversionAssemblyInitiated extends PojaEvent {
   @Override
   public Duration maxConsumerBackoffBetweenRetries() {
     return Duration.ofMinutes(1L);
+  }
+
+  @Override
+  public EventStack getEventStack() {
+    return EVENT_STACK_2;
   }
 }
