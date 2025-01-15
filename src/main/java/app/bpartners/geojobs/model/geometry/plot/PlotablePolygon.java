fe709e38527f0123af95ec0d8abce68511818a30
Lou Andria <lou@hei.school>
2025-01-15 12:01:06 +0100

    feat: draw interior rings of Polygon

--- src/main/java/app/bpartners/geojobs/model/geometry/plot/PlotablePolygon.java
+++ src/main/java/app/bpartners/geojobs/model/geometry/plot/PlotablePolygon.java
@@ -3,6 +3,7 @@ package app.bpartners.geojobs.model.geometry.plot;
 import java.awt.*;
 import java.util.Arrays;
 import lombok.AllArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Polygon;
 
 @AllArgsConstructor
@@ -14,7 +15,13 @@ public class PlotablePolygon implements Plotable {
   @Override
   public void plot(Graphics2D g2d, double scale) {
     g2d.setColor(color);
-    var coordinates = polygon.getExteriorRing().getCoordinates();
+    draw(g2d, polygon.getExteriorRing().getCoordinates(), scale);
+    for (int n = 0; n < polygon.getNumInteriorRing(); n++) {
+      draw(g2d, polygon.getInteriorRingN(n).getCoordinates(), scale);
+    }
+  }
+
+  private static void draw(Graphics2D g2d, Coordinate[] coordinates, double scale) {
     g2d.drawPolygon(
         Arrays.stream(coordinates).mapToInt(c -> (int) (c.x * scale)).toArray(),
         Arrays.stream(coordinates).mapToInt(c -> (int) (c.y * scale)).toArray(),
