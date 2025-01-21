00c97dcc1d6235dcbd76df70c5a7b5cfddc64490
Ryan Andriamahery <ryan@hei.school>
2025-01-21 15:30:10 +0300

    chore: delete empty class GeoJsonConversionJobCreatedServiceIT

--- src/test/java/app/bpartners/geojobs/service/event/GeoJsonConversionJobCreatedServiceIT.java
+++
@@ -1,7 +0,0 @@
-package app.bpartners.geojobs.service.event;
-
-import app.bpartners.geojobs.conf.FacadeIT;
-import org.junit.jupiter.api.Disabled;
-
-@Disabled("TODO")
-class GeoJsonConversionJobCreatedServiceIT extends FacadeIT {}
