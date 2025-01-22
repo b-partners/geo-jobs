d23024cc5477e9a3e9c148d5546f282b6be6bfe0
Ryan Andriamahery <ryan@hei.school>
2025-01-22 14:14:54 +0300

    test: enable TilesDownloaderIT::download_tiles_cannes_ok and drop download_lyon_ok

--- src/test/java/app/bpartners/geojobs/service/geo/TilesDownloaderIT.java
+++ src/test/java/app/bpartners/geojobs/service/geo/TilesDownloaderIT.java
@@ -19,64 +19,14 @@ import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-public class TilesDownloaderIT extends FacadeIT {
+class TilesDownloaderIT extends FacadeIT {
   @MockBean BucketComponent bucketComponent;
   @Autowired TilesDownloader httpApiTilesDownloader;
   @Autowired ObjectMapper om;
-
-  private ParcelContent a_parcel_from_lyon(int zoom)
-      throws MalformedURLException, JsonProcessingException {
-    return ParcelContent.builder()
-        .id(randomUUID().toString())
-        .geoServerUrl(new URL("https://data.grandlyon.com/fr/geoserv/grandlyon/ows"))
-        .geoServerParameter(
-            om.readValue(
-                """
-                {
-                    "service": "WMS",
-                    "request": "GetMap",
-                    "layers": "grandlyon:ortho_2018",
-                    "styles": "",
-                    "format": "image/jpeg",
-                    "version": "1.3.0",
-                    "width": 1024,
-                    "height": 1024,
-                    "srs": "EPSG:3857"
-                }""",
-                GeoServerParameter.class))
-        .feature(
-            om
-                .readValue(
-                    """
-                    { "type": "Feature",
-                      "properties": {
-                        "code": "69",
-                        "nom": "Rhône - 1 sur 1000x100",
-                        "id": 30251921,
-                        "CLUSTER_ID": 99520,
-                        "CLUSTER_SIZE": 386884 },
-                      "geometry": {
-                        "type": "MultiPolygon",
-                        "coordinates": [ [ [
-                          [ 4.803193184300449, 45.732156868763205 ],
-                          [ 4.802538245115325, 45.732990634128193 ],
-                          [ 4.80264872650989, 45.733263461411831 ],
-                          [ 4.803125193613379, 45.733382317920366 ],
-                          [ 4.803576766482497, 45.73258632485657 ],
-                          [ 4.803576472461046, 45.73258224786219 ],
-                          [ 4.803193184300449, 45.732156868763205 ] ] ] ] } }""",
-                    app.bpartners.geojobs.repository.model.Feature.class)
-                .toBuilder()
-                .zoom(zoom)
-                .id("feature_1_id")
-                .build())
-        .build();
-  }
 
   private ParcelContent a_parcel_from_cannes(int zoom)
       throws MalformedURLException, JsonProcessingException {
@@ -135,19 +85,7 @@ public class TilesDownloaderIT extends FacadeIT {
   }
 
   @Test
-  @Disabled("TODO: fail")
-  public void download_tiles_lyon_ok() throws IOException {
-    var zoom = 20;
-
-    var tilesDir = httpApiTilesDownloader.apply(a_parcel_from_lyon(zoom));
-
-    assertEquals(4, new File(tilesDir.getAbsolutePath() + "/" + zoom).listFiles().length);
-  }
-
-  // TODO: run locally only
-  @Test
-  @Disabled
-  public void download_tiles_cannes_ok() throws IOException {
+  void download_tiles_cannes_ok() throws IOException {
     var zoom = 20;
 
     var tilesDir = httpApiTilesDownloader.apply(a_parcel_from_cannes(zoom));
