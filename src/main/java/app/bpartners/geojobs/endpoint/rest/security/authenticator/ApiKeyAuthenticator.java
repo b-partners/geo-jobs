c6888a7ce93418460a8f9b7727ee0b1eb562f617
Ryan Andriamahery <ryan@hei.school>
2025-01-30 14:44:32 +0300

    fix: handle multiple authority roles in Authenticator

--- src/main/java/app/bpartners/geojobs/endpoint/rest/security/authenticator/ApiKeyAuthenticator.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/security/authenticator/ApiKeyAuthenticator.java
@@ -6,8 +6,8 @@ import static app.bpartners.geojobs.endpoint.rest.security.model.Authority.Role.
 import app.bpartners.geojobs.endpoint.rest.security.model.Authority;
 import app.bpartners.geojobs.endpoint.rest.security.model.Principal;
 import app.bpartners.geojobs.repository.CommunityAuthorizationRepository;
+import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -31,12 +31,22 @@ public class ApiKeyAuthenticator implements UsernamePasswordAuthenticator {
   public UserDetails retrieveUser(
       String username, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) {
     String candidateApiKey = getApiKeyFromHeader(usernamePasswordAuthenticationToken);
-    if (existsAsNotRevokedApiKeyInCommunityKeys(candidateApiKey)) {
-      return new Principal(candidateApiKey, Set.of(new Authority(ROLE_COMMUNITY)));
-    } else if (adminApiKey.equals(candidateApiKey)) {
-      return new Principal(candidateApiKey, Set.of(new Authority(ROLE_ADMIN)));
+    HashSet<Authority> authorities = getAuthorities(candidateApiKey);
+    if (!authorities.isEmpty()) {
+      return new Principal(candidateApiKey, authorities);
     }
     throw new BadCredentialsException("Bad credentials");
+  }
+
+  private HashSet<Authority> getAuthorities(String candidateApiKey) {
+    HashSet<Authority> authorities = new HashSet<>();
+    if (adminApiKey.equals(candidateApiKey)) {
+      authorities.add(new Authority(ROLE_ADMIN));
+    }
+    if (existsAsNotRevokedApiKeyInCommunityKeys(candidateApiKey)) {
+      authorities.add(new Authority(ROLE_COMMUNITY));
+    }
+    return authorities;
   }
 
   private boolean existsAsNotRevokedApiKeyInCommunityKeys(String candidateApiKey) {
