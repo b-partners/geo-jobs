e7456bd77ec0153da7e99d9efa09bd04f01d05f7
Ryan Andriamahery <ryan@hei.school>
2025-02-11 13:10:39 +0300

    refactor: remove debug logs for ZoneDetectionJobSucceededService

--- src/main/java/app/bpartners/geojobs/service/event/ZoneDetectionJobSucceededService.java
+++ src/main/java/app/bpartners/geojobs/service/event/ZoneDetectionJobSucceededService.java
@@ -8,13 +8,11 @@ import app.bpartners.geojobs.endpoint.event.model.zone.ZoneDetectionJobSucceeded
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
-@Slf4j
 public class ZoneDetectionJobSucceededService implements Consumer<ZoneDetectionJobSucceeded> {
   public static final double DEFAULT_MINIMUM_CONFIDENCE_FOR_DELIVERY = 0.95;
   private final EventProducer eventProducer;
@@ -22,7 +20,6 @@ public class ZoneDetectionJobSucceededService implements Consumer<ZoneDetectionJ
   @Override
   @Transactional
   public void accept(ZoneDetectionJobSucceeded event) {
-    log.info("ZoneDetectionJobSucceeded {}, now handling human detection job", event);
     var annotationJobWithObjectsIdTruePositive = randomUUID().toString();
     var annotationJobWithObjectsIdFalsePositive = randomUUID().toString();
     var annotationJobWithoutObjectsId = randomUUID().toString();
