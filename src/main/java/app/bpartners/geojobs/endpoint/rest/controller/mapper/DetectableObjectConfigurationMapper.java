278626853566fa756f7fcaaf4b1702d5626e34ab
Ryan Andriamahery <ryan@hei.school>
2025-03-06 16:12:54 +0300

    chore: rename DetectableObjectConfigurationMapper::DEFAULT_CONFIDENCE_FOR_IN_DOUBT_DETECTION

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/DetectableObjectConfigurationMapper.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/DetectableObjectConfigurationMapper.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 public class DetectableObjectConfigurationMapper {
   private final DetectableObjectTypeMapper typeMapper;
   private BucketConf bucketConf;
-  public final int DEFAULT_CONFIDENCE = 1;
+  private final Double DEFAULT_CONFIDENCE_FOR_IN_DOUBT_DETECTION = 1.0;
 
   public DetectableObjectConfiguration toDomain(
       String jobId, app.bpartners.geojobs.endpoint.rest.model.DetectableObjectConfiguration rest) {
@@ -23,7 +23,9 @@ public class DetectableObjectConfigurationMapper {
         .detectionJobId(jobId)
         .objectType(typeMapper.toDomain(Objects.requireNonNull(rest.getType())))
         .minConfidenceForDetection(
-            rest.getConfidence() != null ? rest.getConfidence().doubleValue() : 1)
+            rest.getConfidence() != null
+                ? rest.getConfidence().doubleValue()
+                : DEFAULT_CONFIDENCE_FOR_IN_DOUBT_DETECTION)
         .bucketStorageName(
             rest.getBucketStorageName() != null
                 ? rest.getBucketStorageName()
@@ -36,7 +38,7 @@ public class DetectableObjectConfigurationMapper {
     return new app.bpartners.geojobs.endpoint.rest.model.DetectableObjectConfiguration()
         .confidence(
             domain.getMinConfidenceForDetection() == null
-                ? BigDecimal.valueOf(DEFAULT_CONFIDENCE)
+                ? BigDecimal.valueOf(DEFAULT_CONFIDENCE_FOR_IN_DOUBT_DETECTION)
                 : BigDecimal.valueOf(domain.getMinConfidenceForDetection()))
         .type(typeMapper.toRest(domain.getObjectType()))
         .bucketStorageName(domain.getBucketStorageName());
