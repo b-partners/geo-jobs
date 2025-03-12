326d41616f4d2886edadeb880756600de80fac65
Ryan Andriamahery <ryan@hei.school>
2025-03-12 12:53:51 +0300

    test(ZoneDetectionJobSucceededServiceIT): fix zdj_succeeds_trigger_delivery_job and add zdj_succeeds_trigger_conversion_job

--- src/test/java/app/bpartners/geojobs/service/event/ZoneDetectionJobSucceededServiceIT.java
+++ src/test/java/app/bpartners/geojobs/service/event/ZoneDetectionJobSucceededServiceIT.java
@@ -2,16 +2,17 @@ package app.bpartners.geojobs.service.event;
 
 import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import app.bpartners.geojobs.conf.FacadeIT;
 import app.bpartners.geojobs.endpoint.event.EventProducer;
+import app.bpartners.geojobs.endpoint.event.model.GeoJsonConversionJobCreated;
 import app.bpartners.geojobs.endpoint.event.model.annotation.AnnotationDeliveryJobRequested;
 import app.bpartners.geojobs.endpoint.event.model.zone.ZoneDetectionJobSucceeded;
 import app.bpartners.geojobs.repository.*;
 import app.bpartners.geojobs.repository.model.detection.*;
 import app.bpartners.geojobs.repository.model.tiling.ZoneTilingJob;
+import app.bpartners.geojobs.service.detection.ZoneDetectionJobService;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 class ZoneDetectionJobSucceededServiceIT extends FacadeIT {
   private final String succeededJobId;
   private final String zoneTilingJobId;
+  @MockBean ZoneDetectionJobService zoneDetectionJobService;
   @MockBean EventProducer eventProducer;
   @Autowired ZoneDetectionJobSucceededService subject;
   @Autowired private ZoneDetectionJobRepository jobRepository;
@@ -57,11 +59,24 @@ class ZoneDetectionJobSucceededServiceIT extends FacadeIT {
 
   @Test
   void zdj_succeeds_trigger_delivery_job() {
+    when(zoneDetectionJobService.countInDoubtDetectedTileToDeliveryById(any())).thenReturn(1L);
     assertDoesNotThrow(() -> subject.accept(new ZoneDetectionJobSucceeded(succeededJobId)));
 
     var eventCaptor = ArgumentCaptor.forClass(List.class);
     verify(eventProducer, times(1)).accept(eventCaptor.capture());
     var capturedEvent = eventCaptor.getValue().getFirst();
     assertEquals(AnnotationDeliveryJobRequested.class, capturedEvent.getClass());
+  }
+
+  @Test
+  void zdj_succeeds_trigger_conversion_job() {
+    when(zoneDetectionJobService.countInDoubtDetectedTileToDeliveryById(any())).thenReturn(0L);
+    when(zoneDetectionJobService.findById(any())).thenReturn(new ZoneDetectionJob());
+    assertDoesNotThrow(() -> subject.accept(new ZoneDetectionJobSucceeded(succeededJobId)));
+
+    var eventCaptor = ArgumentCaptor.forClass(List.class);
+    verify(eventProducer, times(1)).accept(eventCaptor.capture());
+    var capturedEvent = eventCaptor.getValue().getFirst();
+    assertEquals(GeoJsonConversionJobCreated.class, capturedEvent.getClass());
   }
 }
