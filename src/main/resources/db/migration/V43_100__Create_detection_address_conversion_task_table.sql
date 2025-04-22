6d560b6a5880926a6554c9ebbc8da7deee4a0030
Ryan Andriamahery <ryan@hei.school>
2025-04-22 16:35:56 +0300

    chore(sql): detection_address_conversion_task table references detection_address_conversion_job

--- src/main/resources/db/migration/V43_100__Create_detection_address_conversion_task_table.sql
+++ src/main/resources/db/migration/V43_100__Create_detection_address_conversion_task_table.sql
@@ -1,7 +1,7 @@
 create table if not exists "detection_address_conversion_task"
 (
     id                 varchar primary key,
-    job_id             varchar references "annotation_delivery_job" (id),
+    job_id             varchar references "detection_address_conversion_job" (id),
     as_job_id          varchar,
     submission_instant timestamp with time zone default now()::timestamp with time zone,
     address            varchar,
