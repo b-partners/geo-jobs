5420a411f61a7c87b432a5e221dd92233aad67b7
Ryan Andriamahery <ryan@hei.school>
2025-04-22 16:37:04 +0300

    chore: produces DetectionAddressConversionJobStatusRecomputingSubmitted inside DetectionAddressConversionJobService::create

--- src/main/java/app/bpartners/geojobs/service/event/DetectionAddressConversionJobCreatedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/DetectionAddressConversionJobCreatedService.java
@@ -1,10 +1,7 @@
 package app.bpartners.geojobs.service.event;
 
-import app.bpartners.geojobs.endpoint.event.EventProducer;
-import app.bpartners.geojobs.endpoint.event.model.DetectionAddressConversionJobStatusRecomputingSubmitted;
 import app.bpartners.geojobs.endpoint.event.model.zone.DetectionAddressConversionJobCreated;
 import app.bpartners.geojobs.service.DetectionAddressConversionJobService;
-import java.util.List;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,17 +11,11 @@ import org.springframework.stereotype.Service;
 public class DetectionAddressConversionJobCreatedService
     implements Consumer<DetectionAddressConversionJobCreated> {
   private final DetectionAddressConversionJobService detectionAddressConversionJobService;
-  private final EventProducer eventProducer;
 
   @Override
   public void accept(DetectionAddressConversionJobCreated event) {
     var detectionAddressConversionJob = event.getJob();
 
     detectionAddressConversionJobService.fireTasks(detectionAddressConversionJob.getId());
-
-    eventProducer.accept(
-        List.of(
-            new DetectionAddressConversionJobStatusRecomputingSubmitted(
-                detectionAddressConversionJob.getId())));
   }
 }
