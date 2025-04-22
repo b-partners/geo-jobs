c0bb73d182e5938a96c0b07826050f88cf9fc5f4
Ryan Andriamahery <ryan@hei.school>
2025-04-22 12:33:34 +0300

    chore: add ExcelAddressConverter using poi-ooxml v5.4.1

---
+++ src/main/java/app/bpartners/geojobs/service/ExcelAddressConverter.java
@@ -0,0 +1,40 @@
+package app.bpartners.geojobs.service;
+
+import static app.bpartners.geojobs.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+
+import app.bpartners.geojobs.model.exception.ApiException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExcelAddressConverter implements Function<File, List<String>> {
+
+  @Override
+  public List<String> apply(File file) {
+    var addresses = new ArrayList<String>();
+    try (FileInputStream fis = new FileInputStream(file);
+        Workbook workbook = new XSSFWorkbook(fis)) {
+      Sheet sheet = workbook.getSheetAt(0);
+      for (Row row : sheet) {
+        if (row.getRowNum() > 0) {
+          for (Cell cell : row) {
+            if (Objects.requireNonNull(cell.getCellType()) == CellType.STRING) {
+              addresses.add(cell.getStringCellValue());
+            }
+          }
+        }
+      }
+      return addresses;
+    } catch (IOException e) {
+      throw new ApiException(SERVER_EXCEPTION, e);
+    }
+  }
+}
