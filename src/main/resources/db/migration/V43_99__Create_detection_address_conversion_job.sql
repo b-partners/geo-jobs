73c1e3150474624a25e41937bb5595915eae4d40
Ryan Andriamahery <ryan@hei.school>
2025-04-22 16:35:12 +0300

    chore(sql): add DETECTION_ADDRESS_CONVERSION job_type

--- src/main/resources/db/migration/V43_99__Create_detection_address_conversion_job.sql
+++ src/main/resources/db/migration/V43_99__Create_detection_address_conversion_job.sql
@@ -6,3 +6,5 @@ create table if not exists "detection_address_conversion_job"
     submission_instant timestamp with time zone default now()::timestamp with time zone,
     detection_id       varchar
 );
+
+alter type job_type add value if not exists 'DETECTION_ADDRESS_CONVERSION';
