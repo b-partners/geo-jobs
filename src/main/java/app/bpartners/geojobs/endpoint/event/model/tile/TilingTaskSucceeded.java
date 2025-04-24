a896c9d7e577c9e542cc17293b1a9f8b3274a099
Ryan Andriamahery <ryan@hei.school>
2025-04-24 23:32:20 +0300

    chore(TilingTaskSucceeded): handle in EVENT_STACK_1

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/tile/TilingTaskSucceeded.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/tile/TilingTaskSucceeded.java
@@ -1,8 +1,5 @@
 package app.bpartners.geojobs.endpoint.event.model.tile;
 
-import static app.bpartners.geojobs.endpoint.event.EventStack.EVENT_STACK_2;
-
-import app.bpartners.geojobs.endpoint.event.EventStack;
 import app.bpartners.geojobs.endpoint.event.model.PojaEvent;
 import app.bpartners.geojobs.repository.model.tiling.ParcelTilingTask;
 import java.time.Duration;
@@ -32,10 +29,5 @@ public class TilingTaskSucceeded extends PojaEvent {
   @Override
   public Duration maxConsumerBackoffBetweenRetries() {
     return Duration.ofMinutes(1);
-  }
-
-  @Override
-  public EventStack getEventStack() {
-    return EVENT_STACK_2;
   }
 }
