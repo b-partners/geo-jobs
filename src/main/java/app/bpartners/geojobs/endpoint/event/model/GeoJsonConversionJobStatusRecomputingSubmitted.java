e37a8a333b25275d01e74f1cb51038597876593f
Ryan Andriamahery <ryan@hei.school>
2025-04-24 23:04:54 +0300

    chore(GeoJsonConversionJobStatusRecomputingSubmitted): set maxConsumerDuration=60s and initialBackOffDuration=30s

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/GeoJsonConversionJobStatusRecomputingSubmitted.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/GeoJsonConversionJobStatusRecomputingSubmitted.java
@@ -15,8 +15,8 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = false)
 @ToString
 public class GeoJsonConversionJobStatusRecomputingSubmitted extends JobStatusRecomputingSubmitted {
-  private static final long MAX_CONSUMER_DURATION_IN_SECONDS = Duration.ofMinutes(5).getSeconds();
-  public static final long INITIAL_BACKOFF_DURATION_IN_SECONDS = Duration.ofMinutes(2).getSeconds();
+  private static final long MAX_CONSUMER_DURATION_IN_SECONDS = Duration.ofSeconds(60).getSeconds();
+  private static final long INITIAL_BACKOFF_DURATION_IN_SECONDS = Duration.ofMinutes(30).getSeconds();
 
   public GeoJsonConversionJobStatusRecomputingSubmitted(String jobId) {
     super(jobId, MAX_CONSUMER_DURATION_IN_SECONDS, INITIAL_BACKOFF_DURATION_IN_SECONDS);
