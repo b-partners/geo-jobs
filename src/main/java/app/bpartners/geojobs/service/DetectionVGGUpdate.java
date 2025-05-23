2f0dcaf9964a82adbc2aa0bd88e6ef06c5b8b5dd
Ryan Andriamahery <ryan@hei.school>
2025-05-23 11:08:17 +0300

    fix: compute vgg_url inside detections provided features

--- src/main/java/app/bpartners/geojobs/service/DetectionVGGUpdate.java
+++ src/main/java/app/bpartners/geojobs/service/DetectionVGGUpdate.java
@@ -3,10 +3,13 @@ package app.bpartners.geojobs.service;
 import static app.bpartners.geojobs.file.FileWriter.createTempDirectory;
 import static app.bpartners.geojobs.service.event.GeoJsonConversionTaskConsumer.GEO_JSON_EXTENSION;
 
+import app.bpartners.geojobs.endpoint.rest.controller.mapper.FeatureMapper;
+import app.bpartners.geojobs.endpoint.rest.model.Feature;
 import app.bpartners.geojobs.file.FileWriter;
 import app.bpartners.geojobs.file.bucket.BucketComponent;
 import app.bpartners.geojobs.model.geometry.VGG;
 import app.bpartners.geojobs.repository.model.detection.Detection;
+import java.util.*;
 import java.util.function.BiFunction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -28,5 +31,46 @@ public class DetectionVGGUpdate implements BiFunction<VGG, Detection, Detection>
         fileWriter.write(vggAsByte, createTempDirectory(), zoneName + GEO_JSON_EXTENSION);
     bucketComponent.upload(vggAsFile, fileKey);
     return detection.toBuilder().vggFileKey(fileKey).build();
+  }
+
+  public Detection apply(Map<Feature, VGG> vgg, Detection detection) {
+    var providedGeoJsonZone = detection.getProvidedGeoJsonZone();
+    var layer = detection.getGeoServerProperties().getGeoServerParameter().getLayers();
+    var updatedGeoJsonZone =
+        providedGeoJsonZone.stream()
+            .map(
+                feature -> {
+                  var optionalVgg =
+                      vgg.entrySet().stream()
+                          .filter(entry -> entry.getKey().equals(feature))
+                          .findAny();
+
+                  if (optionalVgg.isPresent()) {
+                    var longitude = feature.getGeometry().getPoint().getCoordinates().getFirst();
+                    var latitude = feature.getGeometry().getPoint().getCoordinates().getLast();
+
+                    var filename = layer + "/vgg_" + longitude + "_" + latitude;
+                    var fileKey = filename + ".json";
+                    var vggAsByte = optionalVgg.get().getValue().getBytes();
+                    var vggAsFile = fileWriter.write(vggAsByte, createTempDirectory(), filename);
+                    bucketComponent.upload(vggAsFile, fileKey);
+
+                    var propertiesWithVggFileKey =
+                        new HashMap<>(Objects.requireNonNull(feature.getProperties()));
+                    propertiesWithVggFileKey.put("vgg_file_key", fileKey);
+                    return new Feature()
+                        .type(feature.getType())
+                        .geometry(feature.getGeometry())
+                        .properties(propertiesWithVggFileKey);
+                  }
+                  return null;
+                })
+            .filter(Objects::nonNull)
+            .toList();
+
+    return detection.toBuilder()
+        .providedGeoJsonZone(
+            updatedGeoJsonZone.stream().map(FeatureMapper::toDomainFeature).toList())
+        .build();
   }
 }
