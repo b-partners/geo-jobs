c8abad2f8b7e30ef4d74dcb3991b4f18b38fb2df
Rija <Ramilison@MacBook-Pro.local>
2025-05-26 16:13:11 +0300

    chore: test code

--- src/test/java/app/bpartners/geojobs/service/event/ExtendedImageWithDetectedObjectRequestedServiceTest.java
+++ src/test/java/app/bpartners/geojobs/service/event/ExtendedImageWithDetectedObjectRequestedServiceTest.java
@@ -1,26 +1,28 @@
 package app.bpartners.geojobs.service.event;
 
+import static app.bpartners.geojobs.repository.model.ArcgisImageZoom.HOUSES_0;
 import static java.util.UUID.randomUUID;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import app.bpartners.geojobs.endpoint.event.EventProducer;
 import app.bpartners.geojobs.endpoint.event.model.ExtendedImageWithDetectedObjectRequested;
-import app.bpartners.geojobs.endpoint.rest.model.GeoServerParameter;
-import app.bpartners.geojobs.endpoint.rest.model.GeoServerProperties;
+import app.bpartners.geojobs.endpoint.rest.model.*;
 import app.bpartners.geojobs.file.FileWriter;
 import app.bpartners.geojobs.file.bucket.BucketComponent;
 import app.bpartners.geojobs.repository.DetectionRepository;
 import app.bpartners.geojobs.repository.MachineDetectedTileRepository;
 import app.bpartners.geojobs.repository.model.detection.Detection;
+import app.bpartners.geojobs.repository.model.detection.MachineDetectedTile;
 import app.bpartners.geojobs.service.DetectedImageDraw;
 import app.bpartners.geojobs.service.geojson.GeometryConverter;
 import app.bpartners.geojobs.service.tile19.ExtenderApi;
 import app.bpartners.geojobs.service.tiling.TileFinder;
 import app.bpartners.geojobs.service.tiling.TiledPixelPolygonFilter;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+
+import java.math.BigDecimal;
+import java.util.*;
+
 import org.junit.jupiter.api.Test;
 
 class ExtendedImageWithDetectedObjectRequestedServiceTest {
@@ -79,5 +81,75 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
     verify(extenderApiMock, never()).apply(any());
     verify(fileWriterMock, never()).base64ToFile(any(), any());
     verify(bucketComponentMock, never()).upload(any(), any());
+  }
+
+  @Test
+  void throwException_when_detectionNotFound() {
+    var detectionId = randomUUID().toString();
+    ExtendedImageWithDetectedObjectRequested event =
+            new ExtendedImageWithDetectedObjectRequested(detectionId);
+    when(detectionRepositoryMock.findById(detectionId)).thenReturn(Optional.empty());
+
+    var actual = assertThrows(
+            NoSuchElementException.class,
+            () -> subject.accept(event)
+    );
+
+    assertEquals("Exception", actual.getMessage());
+  }
+
+  @Test
+  void detection_has_no_point_delimitation() {
+    var layers = "cite:PCRS";
+    var detectionId = randomUUID().toString();
+    var detectionMock = mock(Detection.class);
+    when(detectionMock.getId()).thenReturn(detectionId);
+    when(detectionMock.getGeoServerProperties())
+            .thenReturn(
+                    new GeoServerProperties().geoServerParameter(new GeoServerParameter().layers(layers)));
+    when(detectionMock.hasOnlyPointsGeoJson()).thenReturn(true);
+    when(detectionMock.getPointDelimitation()).thenReturn(null);
+    when(detectionRepositoryMock.findById(detectionId)).thenReturn(Optional.of(detectionMock));
+
+    assertDoesNotThrow(
+            () -> subject.accept(new ExtendedImageWithDetectedObjectRequested(detectionId)));
+
+    verify(tileFinderMock, never()).getSurroundingTiles(any(), any(), anyInt());
+    verify(detectedTileRepositoryMock, never()).findAllByZdjJobId(any());
+  }
+
+  @Test
+  void processus_complet() {
+    var layers = "cite:PCRS";
+    var detectionId = randomUUID().toString();
+    var zdjId = randomUUID().toString();
+    var detectionMock = mock(Detection.class);
+    when(detectionMock.getId()).thenReturn(detectionId);
+    when(detectionMock.getGeoServerProperties()).thenReturn(new GeoServerProperties().geoServerParameter(new GeoServerParameter().layers(layers)));
+    when(detectionMock.hasOnlyPointsGeoJson()).thenReturn(true);
+    when(detectionMock.getZdjId()).thenReturn(zdjId);
+
+    /// infos du GEOJSON ///
+    var geoJsonPoint = List.of(BigDecimal.valueOf(10.0), BigDecimal.valueOf(20.0));
+    var geoJsonPointFeature = mock(Point.class);
+    when(geoJsonPointFeature.getCoordinates()).thenReturn(geoJsonPoint);
+    var mockedgeoJsonFeatureGeometry = mock(FeatureGeometry.class);
+    when(mockedgeoJsonFeatureGeometry.getPoint()).thenReturn(geoJsonPointFeature);
+    when(mockedgeoJsonFeatureGeometry.getActualInstance()).thenReturn(geoJsonPointFeature);
+
+    var feature = new Feature().geometry(mockedgeoJsonFeatureGeometry);
+    when(detectionMock.getProvidedGeoJsonZone()).thenReturn(List.of(feature));
+
+    /// infos Feature Objet Json ///
+    var featurePointDelimitation = List.of(BigDecimal.valueOf(5.0), BigDecimal.valueOf(5.0));
+    var featureMockedpointDelimitation = mock(Point.class);
+    when(featureMockedpointDelimitation.getCoordinates()).thenReturn(featurePointDelimitation);
+
+    var mockedFeatureGeometry = mock(FeatureGeometry.class);
+    when(mockedFeatureGeometry.getPoint()).thenReturn(featureMockedpointDelimitation);
+    when(mockedFeatureGeometry.getActualInstance()).thenReturn(featureMockedpointDelimitation);
+
+    var tileCoordinates = new TileCoordinates().x(1).y(2).z(HOUSES_0.getZoomLevel());
+
   }
 }
