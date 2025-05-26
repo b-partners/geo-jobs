bcb35a7c8579efcf8943d4974a4e6d12b46629b0
Ryan Andriamahery <ryan@hei.school>
2025-05-26 12:58:10 +0300

    fix(InternalToRestExceptionHandler): explicit error types during HttpMessageNotReadableException

--- src/main/java/app/bpartners/geojobs/endpoint/rest/InternalToRestExceptionHandler.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/InternalToRestExceptionHandler.java
@@ -2,6 +2,9 @@ package app.bpartners.geojobs.endpoint.rest;
 
 import app.bpartners.geojobs.endpoint.rest.model.RestException;
 import app.bpartners.geojobs.model.exception.*;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +39,33 @@ public class InternalToRestExceptionHandler {
 
   @ExceptionHandler(value = {HttpMessageNotReadableException.class})
   ResponseEntity<RestException> handleBadRequest(HttpMessageNotReadableException e) {
+    Throwable cause = e.getCause();
+
+    if (cause instanceof InvalidFormatException ife) {
+      List<String> errors =
+          ife.getPath().stream()
+              .map(
+                  ref ->
+                      "Field '"
+                          + ref.getFieldName()
+                          + "' expects a value of type "
+                          + ife.getTargetType().getSimpleName()
+                          + " but got '"
+                          + ife.getValue()
+                          + "'")
+              .toList();
+
+      return handleBadRequest(new BadRequestException(errors.toString()));
+    }
+
+    if (cause instanceof JsonMappingException jme) {
+      List<String> errors =
+          jme.getPath().stream()
+              .map(ref -> "Problem with field: '" + ref.getFieldName() + "'")
+              .toList();
+
+      return handleBadRequest(new BadRequestException(errors.toString()));
+    }
     log.info("Missing required body", e);
     return handleBadRequest(new BadRequestException(e.getMessage()));
   }
