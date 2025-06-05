ba4b942bee744366e8d268ee24abae3826f38187
Ryan Andriamahery <ryan@hei.school>
2025-06-05 11:23:47 +0300

    chore: format code ExtendedImageWithDetectedObjectRequestedServiceTest

--- src/test/java/app/bpartners/geojobs/service/event/ExtendedImageWithDetectedObjectRequestedServiceTest.java
+++ src/test/java/app/bpartners/geojobs/service/event/ExtendedImageWithDetectedObjectRequestedServiceTest.java
@@ -25,7 +25,6 @@ import app.bpartners.geojobs.service.geojson.GeometryConverter;
 import app.bpartners.geojobs.service.tile19.ExtenderApi;
 import app.bpartners.geojobs.service.tiling.TileFinder;
 import app.bpartners.geojobs.service.tiling.TiledPixelPolygonFilter;
-import java.io.File;
 import java.math.BigDecimal;
 import java.util.*;
 import java.util.NoSuchElementException;
@@ -73,37 +72,12 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
   }
 
   @Test
-  void detection_does_not_have_point_delimitation() {
-    var layers = "cite:PCRS";
-    var detectionId = randomUUID().toString();
-    var detectionMock = mock(Detection.class);
-    when(detectionMock.getId()).thenReturn(detectionId);
-    when(detectionMock.getGeoServerProperties())
-        .thenReturn(
-            new GeoServerProperties().geoServerParameter(new GeoServerParameter().layers(layers)));
-    when(detectionMock.getPointDelimitation()).thenReturn(null);
-    when(detectionRepositoryMock.findById(detectionId)).thenReturn(Optional.of(detectionMock));
-
-    assertDoesNotThrow(
-        () -> subject.accept(new ExtendedImageWithDetectedObjectRequested(detectionId)));
-
-    verify(detectedTileRepositoryMock, times(1)).findAllByZdjJobId(any());
-    verify(tileFinderMock, never()).getSurroundingTiles(any(), any(), anyInt());
-    verify(bucketComponentMock, never()).download(any());
-    verify(detectedImageDrawMock, never()).apply(any(), any());
-    verify(extenderApiMock, never()).apply(any());
-    verify(fileWriterMock, never()).base64ToFile(any(), any());
-    verify(bucketComponentMock, never()).upload(any(), any());
-  }
-
-  @Test
-  void
-      detection_with_point_delimitation_convert_tiled_pixel_polygon_and_produces_detection_vgg_requested() {
+  void detection_produces_detection_vgg_requested() {
     var layers = "cite:PCRS";
     var detectionId = randomUUID().toString();
     var zdjId = randomUUID().toString();
     var detectionMock = mock(Detection.class);
-    var providedFeaturePointMock = mock(Feature.class);
+    var providedFeatureMock = mock(Feature.class);
     var machineDetectedTileMock = mock(MachineDetectedTile.class);
     var pointMock = mock(Point.class);
     var featureGeometryMock = mock(FeatureGeometry.class);
@@ -119,21 +93,17 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
     var featureDetectedObjectMock = mock(Feature.class);
     var detectedFeatureGeometryMock = mock(FeatureGeometry.class);
     var detectedMultiPolygonMock = mock(MultiPolygon.class);
-    var xPixel = BigDecimal.valueOf(0);
-    var yPixel = BigDecimal.valueOf(0);
     var detectedPolygonMock = mock(org.locationtech.jts.geom.Polygon.class);
-    HashMap<Feature, Feature> pointDelimitation = new HashMap<>();
     var toitureConvertedMultiPolygonMock = mock(org.locationtech.jts.geom.MultiPolygon.class);
     var featureGeometryToitureMultiPolygonMock = mock(FeatureGeometry.class);
     var toitureMultiPolygonMock = mock(MultiPolygon.class);
+
     when(detectionMock.getId()).thenReturn(detectionId);
     when(detectionMock.getGeoServerProperties())
         .thenReturn(
             new GeoServerProperties().geoServerParameter(new GeoServerParameter().layers(layers)));
-    when(detectionMock.hasOnlyPointsGeoJson()).thenReturn(true);
     when(detectionMock.getZdjId()).thenReturn(zdjId);
-    when(detectionMock.getProvidedGeoJsonZone()).thenReturn(List.of(providedFeaturePointMock));
-    when(detectionMock.getPointDelimitation()).thenReturn(pointDelimitation);
+    when(detectionMock.getProvidedGeoJsonZone()).thenReturn(List.of(providedFeatureMock));
 
     when(tileCoordinatesMock.getX()).thenReturn(xTile);
     when(tileCoordinatesMock.getY()).thenReturn(yTile);
@@ -150,7 +120,8 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
 
     when(pointMock.getCoordinates()).thenReturn(List.of(longitude, latitude));
     when(featureGeometryMock.getPoint()).thenReturn(pointMock);
-    when(providedFeaturePointMock.getGeometry()).thenReturn(featureGeometryMock);
+    when(providedFeatureMock.getProperties()).thenReturn(Map.of("centroid", pointMock));
+    when(providedFeatureMock.getGeometry()).thenReturn(featureGeometryMock);
     when(featureGeometryMock.getActualInstance()).thenReturn(pointMock);
     when(featureGeometryToitureMultiPolygonMock.getActualInstance())
         .thenReturn(toitureMultiPolygonMock);
@@ -158,7 +129,6 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
         .thenReturn(toitureMultiPolygonMock);
     when(featureToitureMultiPolygon.getGeometry())
         .thenReturn(featureGeometryToitureMultiPolygonMock);
-    pointDelimitation.put(providedFeaturePointMock, featureToitureMultiPolygon);
     when(detectionRepositoryMock.findById(detectionId)).thenReturn(Optional.of(detectionMock));
     when(detectedTileRepositoryMock.findAllByZdjJobId(zdjId))
         .thenReturn(List.of(machineDetectedTileMock));
@@ -170,24 +140,19 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
         .thenAnswer(invocation -> invocation.getArgument(0));
     var multiPolygonStringValue = "multiPolygonStringValue";
     when(geometryConverterMock.writeGeometryAsString(any())).thenReturn(multiPolygonStringValue);
-    when(bucketComponentMock.download(any())).thenReturn(mock(File.class));
-    when(geometryConverterMock.polygonToPoints(any())).thenReturn(List.of(List.of(xPixel, yPixel)));
-    when(detectedImageDrawMock.apply(any(), any())).thenReturn(mock(File.class));
+
     assertDoesNotThrow(
-        () -> subject.accept(new ExtendedImageWithDetectedObjectRequested(detectionId)));
+        () -> subject.accept(new ExtendedImageWithDetectedObjectRequested(detectionId, false)));
+
     verify(detectedTileRepositoryMock, times(1)).findAllByZdjJobId(zdjId);
     verify(tileFinderMock, times(1)).getSurroundingTiles(any(), any(), anyInt());
     verify(geometryConverterMock, times(1)).writeGeometryAsString(any());
-    verify(extenderApiMock, times(1)).apply(any());
-    verify(fileWriterMock, times(1)).base64ToFile(any(), any());
-    verify(bucketComponentMock).download(any());
-    verify(bucketComponentMock).upload(any(), any());
     var listCaptor = ArgumentCaptor.forClass(List.class);
     verify(eventProducerMock, times(1)).accept(listCaptor.capture());
     DetectionVGGRequested event = (DetectionVGGRequested) listCaptor.getValue().getFirst();
     assertEquals(
         expectedVggRequested(
-            detectionId, providedFeaturePointMock, multiPolygonStringValue, xTile, yTile, z),
+            detectionId, providedFeatureMock, multiPolygonStringValue, xTile, yTile, z),
         event);
   }
 
@@ -208,40 +173,5 @@ class ExtendedImageWithDetectedObjectRequestedServiceTest {
                 xTile,
                 yTile,
                 z)));
-  }
-
-  @Test
-  void detection_with_null_point_delimitation_skip() {
-    var layers = "cite:PCRS";
-    var detectionId = randomUUID().toString();
-    var zdjId = randomUUID().toString();
-    var detectionMock = mock(Detection.class);
-    when(detectionMock.getId()).thenReturn(detectionId);
-    when(detectionMock.getGeoServerProperties())
-        .thenReturn(
-            new GeoServerProperties().geoServerParameter(new GeoServerParameter().layers(layers)));
-    when(detectionMock.hasOnlyPointsGeoJson()).thenReturn(true);
-    when(detectionMock.getZdjId()).thenReturn(zdjId);
-
-    when(detectionMock.getPointDelimitation()).thenReturn(null);
-    when(detectionRepositoryMock.findById(detectionId)).thenReturn(Optional.of(detectionMock));
-
-    assertDoesNotThrow(
-        () -> subject.accept(new ExtendedImageWithDetectedObjectRequested(detectionId)));
-
-    verify(detectedTileRepositoryMock, times(1)).findAllByZdjJobId(zdjId);
-    verify(geometryConverterMock, never()).apply(any());
-    verify(tileFinderMock, never()).getSurroundingTiles(any(), any(), anyInt());
-    verify(tiledPixelPolygonFilterMock, never()).filterPolygonsInMask(any(), any());
-    verify(geometryConverterMock, never()).writeGeometryAsString(any());
-    verify(bucketComponentMock, never()).download(any());
-    verify(extenderApiMock, never()).apply(any());
-    verify(fileWriterMock, never()).base64ToFile(any(), any());
-    verify(bucketComponentMock, never()).upload(any(), any());
-    var listCaptor = ArgumentCaptor.forClass(List.class);
-    verify(eventProducerMock, times(1)).accept(listCaptor.capture());
-    DetectionVGGRequested event = (DetectionVGGRequested) listCaptor.getValue().getFirst();
-    assertEquals(detectionId, event.getDetectionId());
-    assertTrue(event.getFilteredTiledPixelPolygons().isEmpty());
   }
 }
