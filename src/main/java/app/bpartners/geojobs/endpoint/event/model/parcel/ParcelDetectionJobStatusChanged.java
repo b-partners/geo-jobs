a2e03845f2a5598d7920502542edead2df32b401
Ryan Andriamahery <ryan@hei.school>
2025-07-08 13:21:50 +0300

    chore(ParcelDetectionJobStatusChanged): handle with EVENT_STACK_2

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/parcel/ParcelDetectionJobStatusChanged.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/parcel/ParcelDetectionJobStatusChanged.java
@@ -1,5 +1,8 @@
 package app.bpartners.geojobs.endpoint.event.model.parcel;
 
+import static app.bpartners.geojobs.endpoint.event.EventStack.EVENT_STACK_2;
+
+import app.bpartners.geojobs.endpoint.event.EventStack;
 import app.bpartners.geojobs.endpoint.event.model.PojaEvent;
 import app.bpartners.geojobs.repository.model.detection.ParcelDetectionJob;
 import java.time.Duration;
@@ -23,5 +26,10 @@ public class ParcelDetectionJobStatusChanged extends PojaEvent {
   @Override
   public Duration maxConsumerBackoffBetweenRetries() {
     return Duration.ofMinutes(1L);
+  }
+
+  @Override
+  public EventStack getEventStack() {
+    return EVENT_STACK_2;
   }
 }
