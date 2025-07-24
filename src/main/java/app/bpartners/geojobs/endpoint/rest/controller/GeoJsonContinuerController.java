fd7c7770338a21a1cf22af305b364a0bfbfb42f7
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-07-24 16:09:39 +0300

    feat: add S3 upload and presigned URL to GeoJSON continuation

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerController.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerController.java
@@ -2,6 +2,7 @@ package app.bpartners.geojobs.endpoint.rest.controller;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.Geojson;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.mapper.FileMapper;
+import app.bpartners.geojobs.file.bucket.BucketComponent;
 import app.bpartners.geojobs.service.geojson.GeoJsonContinuerService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 
-
 @AllArgsConstructor
 @RestController
 public class GeoJsonContinuerController {
     private final GeoJsonContinuerService geoJsonContinuerService;
     private final FileMapper fileMapper;
+    private final BucketComponent bucketComponent;
 
     @GetMapping("/continue")
-    public Geojson continueGeoJson(@RequestBody File geojson) {
-        Geojson geojsonTocontinue = fileMapper.apply(geojson);
+    public String continueGeoJson(@RequestBody File geojsonInput) {
+        var geojsonToContinue = fileMapper.apply(geojsonInput);
+        var result = geoJsonContinuerService.continueGeojson(geojsonToContinue);
 
-        return geoJsonContinuerService.continueGeojson(geojsonTocontinue);
+        var fileName = "geojson-result-" + System.currentTimeMillis() + ".geojson";
+        var tempPath = System.getProperty("java.io.tmpdir") + "/" + fileName;
+
+        result.saveAsFile(tempPath);
+        File outputFile = new File(tempPath);
+
+        String s3Key = "geojson/results/" + fileName;
+        bucketComponent.upload(outputFile, s3Key);
+
+        return bucketComponent.presign(s3Key);
     }
 }
-
