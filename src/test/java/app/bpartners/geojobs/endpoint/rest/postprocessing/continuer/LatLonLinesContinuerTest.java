2c649ddabe30cae95e57dab17ce66373f482dd63
MitsoaAllan <hei.mitsoa@gmail.com>
2025-07-25 12:51:47 +0300

    test: Add more test for LatLonLinesContinuer

--- src/test/java/app/bpartners/geojobs/endpoint/rest/postprocessing/continuer/LatLonLinesContinuerTest.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/postprocessing/continuer/LatLonLinesContinuerTest.java
@@ -1,13 +1,17 @@
 package app.bpartners.geojobs.endpoint.rest.postprocessing.continuer;
 
 import static java.lang.Math.PI;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import app.bpartners.geojobs.endpoint.rest.postprocessing.Geojson;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
 import app.bpartners.geojobs.model.geometry.quadrilateral.model.AlphaConf;
 import app.bpartners.geojobs.model.geometry.route.ContinuationConf;
 import app.bpartners.geojobs.model.geometry.route.PrettyConf;
 import app.bpartners.geojobs.model.geometry.route.RoutesContinuationConf;
 import app.bpartners.geojobs.model.geometry.route.UnionConf;
+import app.bpartners.geojobs.service.geojson.GeoJsonContinuerService;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -16,6 +20,7 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 class LatLonLinesContinuerTest {
+  GeoJsonContinuerService geoJsonContinuerService = new GeoJsonContinuerService();
 
   @Test
   void continue_ivandry() throws IOException, URISyntaxException {
@@ -31,6 +36,28 @@ class LatLonLinesContinuerTest {
         Paths.get(getClass().getResource("/ivandry/route-ivandry-continued.geojson").toURI());
     var expected = Files.readString(expectedURI);
     // assertEquals(expected, new Geojson(continued).stringValue());
+  }
+
+  @Test
+  void continue_amboditsiry() throws IOException, URISyntaxException {
+    var actualGeojson =
+        new File(getClass().getResource("/amboditsiry/route-amboditsiry.geojson").getFile());
+
+    var routesContinuationConf = routesContinuationConf();
+    var tilingConf = new TilingConf(17, 1_024);
+    var latLongLinesContinuer = new LatLonLinesContinuer(routesContinuationConf, tilingConf, 10);
+    var continued = latLongLinesContinuer.apply(actualGeojson);
+    var expectedURI =
+        Paths.get(
+            getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").toURI());
+    var expectedGeojson =
+        new File(
+            getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").getFile());
+    var expected = new Geojson(expectedGeojson);
+    var expectedContinued = Files.readString(expectedURI);
+
+    assertTrue(continued.size() <= expected.polygons().size());
+    assertEquals(expectedContinued, new Geojson(continued).stringValue());
   }
 
   private static RoutesContinuationConf routesContinuationConf() {
