8cc338ec048683fde95db438c73de902e0c315a9
Donner <152691192+DOnn3r@users.noreply.github.com>
2025-07-29 19:55:27 +0300

    fix: responsability

--- src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonContinuerService.java
+++ src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonContinuerService.java
@@ -1,43 +1,56 @@
 package app.bpartners.geojobs.service.geojson;
 
-import static app.bpartners.geojobs.model.continuationConf.LatLonLinesContinuer.*;
-import static app.bpartners.geojobs.model.continuationConf.RoutesContinuationConf.*;
 import static java.lang.Math.PI;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.Geojson;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.continuer.LatLonLinesContinuer;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.LatLonPolygon;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
+import app.bpartners.geojobs.file.bucket.BucketComponent;
 import app.bpartners.geojobs.model.geometry.quadrilateral.model.AlphaConf;
 import app.bpartners.geojobs.model.geometry.route.ContinuationConf;
 import app.bpartners.geojobs.model.geometry.route.PrettyConf;
 import app.bpartners.geojobs.model.geometry.route.RoutesContinuationConf;
 import app.bpartners.geojobs.model.geometry.route.UnionConf;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @Service
 public class GeoJsonContinuerService {
-  private final RoutesContinuationConf routesContinuationConf = routesContinuationConfVal();
+  private final RoutesContinuationConf routesContinuationConf = routesContinuationConf();
   private final LatLonLinesContinuer latLonLinesContinuer =
-      new LatLonLinesContinuer(routesContinuationConf, tilingConfVal(), DEFAULT_NEIGHBOURHOOD.getValue());
+      new LatLonLinesContinuer(routesContinuationConf, new TilingConf(17, 1_024), 10);
+  private final BucketComponent bucketComponent;
 
   public Geojson continueGeojson(File geoJsonToContinue) {
     Set<LatLonPolygon> features = latLonLinesContinuer.apply(geoJsonToContinue);
     return new Geojson(features);
   }
 
-  private static RoutesContinuationConf routesContinuationConfVal() {
-    var alphaConf = new AlphaConf(DEFAULT_MIN_COVERAGE_ABS_AREA.getValue(), DEFAULT_MIN_ABS_AREA.getValue());
-    var unionConf = new UnionConf((int) DEFAULT_BUFFER.getValue());
-    var continuationConf = new ContinuationConf(DEFAULT_MIN_DIRECTION_THRESHOLD.getValue(), DEFAULT_MAX_DIRECTION_THRESHOLD.getValue(), DEFAULT_DISTANCE_THRESHOLD.getValue());
-    var prettyConf = new PrettyConf(DEFAULT_PRETTY_CONF.getValue());
+  private static RoutesContinuationConf routesContinuationConf() {
+    var alphaConf = new AlphaConf(0.5, 1);
+    var unionConf = new UnionConf(1);
+    var continuationConf = new ContinuationConf(PI / 12, PI / 6, 500);
+    var prettyConf = new PrettyConf(0);
     return new RoutesContinuationConf(alphaConf, unionConf, continuationConf, prettyConf);
   }
-  private static TilingConf tilingConfVal(){
-      return new TilingConf(DEFAULT_Z.getValue(), DEFAULT_IMG_SIZE.getValue());
+
+  public String generatePresignedUrl(MultipartFile file) throws IOException {
+    File tempInput = File.createTempFile("geojson-input-", ".geojson");
+    file.transferTo(tempInput);
+
+    var result = continueGeojson(tempInput);
+    File tempOutput = File.createTempFile("geojson-output-", ".geojson");
+    Files.writeString(tempOutput.toPath(), result.toString());
+
+    String bucketKey = "geojson/result/" + tempOutput.getName();
+    bucketComponent.upload(tempOutput, bucketKey);
+    return bucketComponent.presign(bucketKey);
   }
 }
