12aee64af8a1296f7d240c4c14fc5a6768e648d4
Abega1642 <hei.abega.2@gmail.com>
2025-07-29 16:54:20 +0300

    chore: change RoadContinuerService implementation

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerController.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerController.java
@@ -3,7 +3,6 @@ package app.bpartners.geojobs.endpoint.rest.controller;
 import app.bpartners.geojobs.service.RoadContinuerService;
 import java.util.Map;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @RestController
 public class RoadContinuerController {
 
@@ -21,7 +19,6 @@ public class RoadContinuerController {
   @PostMapping("/road-continuer")
   public Map<String, String> roadContinuer(
       @RequestBody String geojson, @RequestParam Integer zoom, @RequestParam Integer imageSize) {
-    var tilingConf = roadContinuerService.getTilingConf(zoom, imageSize);
-    return roadContinuerService.continueRoute(geojson, tilingConf);
+    return roadContinuerService.continueRoute(geojson, zoom, imageSize);
   }
 }
