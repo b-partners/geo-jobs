27dbe0d82a9bd4aee942f671e751854361a76ea4
Abega1642 <hei.abega.2@gmail.com>
2025-07-29 13:44:27 +0300

    chore: change RoadContinuerController implementation

--- src/test/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerControllerTest.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerControllerTest.java
@@ -5,50 +5,44 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
-import app.bpartners.geojobs.service.BucketServiceRoadContinuer;
 import app.bpartners.geojobs.service.RoadContinuerService;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class RoadContinuerControllerTest {
 
   @Mock private RoadContinuerService roadContinuerService;
 
-  @Mock private BucketServiceRoadContinuer bsrc;
-
-  @InjectMocks private RoadContinuerController controller;
-
-  @BeforeEach
-  void setUp() {
-    ReflectionTestUtils.setField(controller, "adminApiKey", "dummyApiKey");
-  }
+  @InjectMocks private RoadContinuerController subject;
 
   @Test
-  void testRoadContinuer_shouldReturnPresignedUrl() throws Exception {
-    String geojsonInput = "{\"type\":\"FeatureCollection\",\"features\":[]}";
-    Integer zoom = 12;
+  void roadContinuer_should_return_presignedUrl() throws Exception {
+    var resource = getClass().getResource("/geojson/quai-de-bourbon.geojson");
+    assertNotNull(resource);
+    var file = new File(resource.toURI());
+    String geojsonInput = Files.readString(file.toPath(), StandardCharsets.UTF_8);
+
+    Integer zoom = 17;
     Integer imageSize = 1_024;
-    File mockGeoJsonFile = new File("mock.geojson");
+
     var tilingConf = new TilingConf(zoom, imageSize);
-    var expected = Map.of("url", "https://presigned.url/mock.geojson");
+    var expected = Map.of("url", "https://moked/qua-de-bourbon-continued.geojson");
 
     when(roadContinuerService.getTilingConf(zoom, imageSize)).thenReturn(tilingConf);
-    when(roadContinuerService.continueRoute(geojsonInput, tilingConf)).thenReturn(mockGeoJsonFile);
-    when(bsrc.getContinuedRoutePresignedUrl(mockGeoJsonFile, "dummyApiKey")).thenReturn(expected);
+    when(roadContinuerService.continueRoute(geojsonInput, tilingConf)).thenReturn(expected);
 
-    var actual = controller.roadContinuer(geojsonInput, zoom, imageSize);
+    var actual = subject.roadContinuer(geojsonInput, zoom, imageSize);
 
     assertEquals(expected, actual);
     verify(roadContinuerService).getTilingConf(zoom, imageSize);
     verify(roadContinuerService).continueRoute(geojsonInput, tilingConf);
-    verify(bsrc).getContinuedRoutePresignedUrl(mockGeoJsonFile, "dummyApiKey");
   }
 }
