aad71c0c5da9814148312acce70e32ff73d54f62
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-08-19 23:31:11 +0300

    fix: test_continue_geojson_controller() to handle byte[] not multipartfile

--- src/test/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerControllerIT.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerControllerIT.java
@@ -7,25 +7,20 @@ import app.bpartners.geojobs.conf.FacadeIT;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockMultipartFile;
 
 @Slf4j
 public class GeoJsonContinuerControllerIT extends FacadeIT {
   @MockBean GeoJsonContinuerController subject;
-  private static final Integer imgSize = 1_024;
-  private static final Integer zoom = 17;
 
   @Test
   void test_continue_geojson_controller() throws Exception {
+    var input = "{}".getBytes();
 
-    MockMultipartFile file =
-        new MockMultipartFile("file", "input.geojson", "application/json", "{}".getBytes());
+    when(subject.continueGeoJson(input)).thenReturn("https://url-presigned");
 
-    when(subject.continueGeoJson(file, imgSize, zoom)).thenReturn("https://url-presigned");
+    String resultUrl = subject.continueGeoJson(input);
 
-    String resultUrl = subject.continueGeoJson(file, imgSize, zoom);
-
-    verify(subject).continueGeoJson(file, imgSize, zoom);
+    verify(subject).continueGeoJson(input);
 
     assertEquals("https://url-presigned", resultUrl);
   }
