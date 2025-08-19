687934be4376dfaf4d2280c649287b94dbb5e8e8
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-08-19 23:24:40 +0300

    fix: rm imageSize, zoom and GeojsonValidator call

--- src/test/java/app/bpartners/geojobs/endpoint/rest/postprocessing/continuer/LatLonLinesContinuerTest.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/postprocessing/continuer/LatLonLinesContinuerTest.java
@@ -24,75 +24,75 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class LatLonLinesContinuerTest {
-  private final BucketComponent bucketComponentMock = Mockito.mock(BucketComponent.class);
-  private final FileWriter fileWriterMock = Mockito.mock(FileWriter.class);
-  private final EventProducer eventProducerMock = Mockito.mock(EventProducer.class);
+    private final BucketComponent bucketComponentMock = Mockito.mock(BucketComponent.class);
+    private final FileWriter fileWriterMock = Mockito.mock(FileWriter.class);
+    private final EventProducer eventProducerMock = Mockito.mock(EventProducer.class);
 
-  @Test
-  void continue_ivandry() throws IOException, URISyntaxException {
-    var ivandryGeojson =
-        new File(getClass().getResource("/ivandry/route-ivandry.geojson").getFile());
+    @Test
+    void continue_ivandry() throws IOException, URISyntaxException {
+        var ivandryGeojson =
+                new File(getClass().getResource("/ivandry/route-ivandry.geojson").getFile());
 
-    var routesContinuationConf = routesContinuationConf();
-    var tilingConf = new TilingConf(17, 1_024);
-    var latLonLinesContinuer = new LatLonLinesContinuer(routesContinuationConf, tilingConf, 10);
-    var continued = latLonLinesContinuer.apply(ivandryGeojson);
+        var routesContinuationConf = routesContinuationConf();
+        var tilingConf = new TilingConf(17, 1_024);
+        var latLonLinesContinuer = new LatLonLinesContinuer(routesContinuationConf, tilingConf, 10);
+        var continued = latLonLinesContinuer.apply(ivandryGeojson);
 
-    var expectedURI =
-        Paths.get(getClass().getResource("/ivandry/route-ivandry-continued.geojson").toURI());
-    var expected = Files.readString(expectedURI);
-    // assertEquals(expected, new Geojson(continued).stringValue());
-  }
+        var expectedURI =
+                Paths.get(getClass().getResource("/ivandry/route-ivandry-continued.geojson").toURI());
+        var expected = Files.readString(expectedURI);
+        // assertEquals(expected, new Geojson(continued).stringValue());
+    }
 
-  // Test with the provided service from GeoJobs, it is the same as continue_ivandy but with
-  // different geoJson
-  @Test
-  void continue_amboditsiry() throws IOException, URISyntaxException {
-    var actualGeojson =
-        new File(getClass().getResource("/amboditsiry/route-amboditsiry.geojson").getFile());
+    // Test with the provided service from GeoJobs, it is the same as continue_ivandy but with
+    // different geoJson
+    @Test
+    void continue_amboditsiry() throws IOException, URISyntaxException {
+        var actualGeojson =
+                new File(getClass().getResource("/amboditsiry/route-amboditsiry.geojson").getFile());
 
-    var routesContinuationConf = routesContinuationConf();
-    var tilingConf = new TilingConf(17, 1_024);
-    var latLongLinesContinuer = new LatLonLinesContinuer(routesContinuationConf, tilingConf, 10);
-    var continued = latLongLinesContinuer.apply(actualGeojson);
-    var expectedURI =
-        Paths.get(
-            getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").toURI());
-    var expectedGeojson =
-        new File(
-            getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").getFile());
-    var expected = new Geojson(expectedGeojson);
-    var expectedContinued = Files.readString(expectedURI);
+        var routesContinuationConf = routesContinuationConf();
+        var tilingConf = new TilingConf(17, 1_024);
+        var latLongLinesContinuer = new LatLonLinesContinuer(routesContinuationConf, tilingConf, 10);
+        var continued = latLongLinesContinuer.apply(actualGeojson);
+        var expectedURI =
+                Paths.get(
+                        getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").toURI());
+        var expectedGeojson =
+                new File(
+                        getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").getFile());
+        var expected = new Geojson(expectedGeojson);
+        var expectedContinued = Files.readString(expectedURI);
 
-    assertTrue(continued.size() <= expected.polygons().size());
-    assertEquals(expectedContinued, new Geojson(continued).stringValue());
-  }
+        assertTrue(continued.size() <= expected.polygons().size());
+        assertEquals(expectedContinued, new Geojson(continued).stringValue());
+    }
 
-  private static RoutesContinuationConf routesContinuationConf() {
-    var alphaConf = new AlphaConf(0.5, 1);
-    var unionConf = new UnionConf(1);
-    var continuationConf = new ContinuationConf(PI / 12, PI / 6, 500);
-    var prettyConf = new PrettyConf(0);
-    return new RoutesContinuationConf(alphaConf, unionConf, continuationConf, prettyConf);
-  }
+    private static RoutesContinuationConf routesContinuationConf() {
+        var alphaConf = new AlphaConf(0.5, 1);
+        var unionConf = new UnionConf(1);
+        var continuationConf = new ContinuationConf(PI / 12, PI / 6, 500);
+        var prettyConf = new PrettyConf(0);
+        return new RoutesContinuationConf(alphaConf, unionConf, continuationConf, prettyConf);
+    }
 
-  // Test with our own service
-  @Test
-  void continue_service_test() throws IOException, URISyntaxException {
-    GeoJsonContinuerService geoJsonContinuerService =
-        new GeoJsonContinuerService(
-            bucketComponentMock, fileWriterMock, eventProducerMock);
+    // Test with our own service
+    @Test
+    void continue_service_test() throws IOException, URISyntaxException {
+        GeoJsonContinuerService geoJsonContinuerService =
+                new GeoJsonContinuerService(
+                        bucketComponentMock, fileWriterMock, eventProducerMock);
 
-    File input =
-        new File(getClass().getResource("/amboditsiry/route-amboditsiry.geojson").getFile());
-    var expectedURI =
-        Paths.get(
-            getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").toURI());
+        File input =
+                new File(getClass().getResource("/amboditsiry/route-amboditsiry.geojson").getFile());
+        var expectedURI =
+                Paths.get(
+                        getClass().getResource("/amboditsiry/route-amboditsiry-continued.geojson").toURI());
 
-    var actual = geoJsonContinuerService.continueGeojson(input);
-    var expectedContinued = Files.readString(expectedURI);
+        var actual = geoJsonContinuerService.continueGeojson(input);
+        var expectedContinued = Files.readString(expectedURI);
 
-    assertEquals(expectedContinued, actual.stringValue());
-    assertTrue(actual.polygons().size() > 0);
-  }
+        assertEquals(expectedContinued, actual.stringValue());
+        assertTrue(actual.polygons().size() > 0);
+    }
 }
