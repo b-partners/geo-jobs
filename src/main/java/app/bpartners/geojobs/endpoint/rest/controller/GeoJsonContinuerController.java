9110e7a6b8568a5d00f770434587ebdaae482cc4
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-19 20:51:35 +0300

    refactor(controller): MutlipartFile into byte[]

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerController.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/GeoJsonContinuerController.java
@@ -1,24 +1,24 @@
 package app.bpartners.geojobs.endpoint.rest.controller;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.GeoJsonValidator;
+import app.bpartners.geojobs.file.FileWriter;
 import app.bpartners.geojobs.service.geojson.GeoJsonContinuerService;
-import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @RestController
 public class GeoJsonContinuerController {
   private final GeoJsonContinuerService geoJsonContinuerService;
   private final GeoJsonValidator geoJsonValidator;
+  private final FileWriter fileWriter;
 
   @PostMapping(value = "/continue")
   public String continueGeoJson(
-      @RequestParam("file") MultipartFile file,
+      @RequestParam("file") byte[] fileToUpload,
       @RequestParam("imageSize") Integer imgSize,
-      @RequestParam("zoom") Integer zoom)
-      throws IOException {
+      @RequestParam("zoom") Integer zoom) {
+    var file = fileWriter.apply(fileToUpload, null);
     geoJsonValidator.accept(file);
     return geoJsonContinuerService.generatePresignedUrl(file, imgSize, zoom);
   }
