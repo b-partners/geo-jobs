1f3658f8a540bf5fbb009e4194591be0f7e8692b
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-20 18:30:11 +0300

    fix: rename file and fix sql syntaxe

--- src/main/resources/db/migration/V43_114__Create_geo_json_continuation_table.sql
+++ src/main/resources/db/migration/V43_114__Create_geo_json_continuation_table.sql
@@ -1,5 +1,5 @@
 create table if not exists "geo_json_continuation" (
     id varchar primary key,
     file_key varchar,
-    status progression_status not null default 'pending';
+    status progression_status not null default 'PENDING'
 )
