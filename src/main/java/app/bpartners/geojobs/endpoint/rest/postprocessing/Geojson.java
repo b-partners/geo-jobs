3c1d69a1081a97d3ea9302fb511986bcf40942bd
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-20 20:40:07 +0300

    fix(model): handle label value

--- src/main/java/app/bpartners/geojobs/endpoint/rest/postprocessing/Geojson.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/postprocessing/Geojson.java
@@ -68,7 +68,10 @@ public class Geojson {
         feature.getProperty("confidence") == null
             ? null
             : feature.getProperty("confidence").getValue();
-    userData.put("label", feature.getProperty("label").getValue());
+    var label = feature.getProperty("label") == null
+            ? null
+            : feature.getProperty("label").getValue();
+    userData.put("label", label);
     userData.put("confidence", confidence);
     Polygon polygon;
     try {
