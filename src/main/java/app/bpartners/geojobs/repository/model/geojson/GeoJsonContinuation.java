1a4d100abc6e4949cddf59c744de6de15252bd7c
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-20 10:01:48 +0300

    feat(repository): model for database

---
+++ src/main/java/app/bpartners/geojobs/repository/model/geojson/GeoJsonContinuation.java
@@ -0,0 +1,29 @@
+package app.bpartners.geojobs.repository.model.geojson;
+
+
+import app.bpartners.geojobs.job.model.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import static org.hibernate.type.SqlTypes.NAMED_ENUM;
+
+@Entity
+@Table(name = "geo_json_continuation")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
+public class GeoJsonContinuation{
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    @Column(name = "file_key")
+    private String fileyKey;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(NAMED_ENUM)
+    private Status.ProgressionStatus status;
+}
