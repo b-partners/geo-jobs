05fc59f0c188c8b211a9cb826ab8fa76a58cf02b
MitsoaAllan <hei.mitsoa@gmail.com>
2025-08-20 10:02:50 +0300

    feat(repository): repository for geojson continuation

---
+++ src/main/java/app/bpartners/geojobs/repository/GeoJsonContinuationRepository.java
@@ -0,0 +1,9 @@
+package app.bpartners.geojobs.repository;
+
+import app.bpartners.geojobs.repository.model.geojson.GeoJsonContinuation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GeoJsonContinuationRepository extends JpaRepository<GeoJsonContinuation, String> {
+}
