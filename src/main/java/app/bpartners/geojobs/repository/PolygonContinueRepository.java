a37326c61b58b08a0aaa9cfc2377842e70711232
juninho <hei.juninho.2@gmail.com>
2025-08-21 12:18:53 +0300

    feat: add repository for Polygon continue

---
+++ src/main/java/app/bpartners/geojobs/repository/PolygonContinueRepository.java
@@ -0,0 +1,7 @@
+package app.bpartners.geojobs.repository;
+
+import app.bpartners.geojobs.repository.model.PolygonContinue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PolygonContinueRepository extends JpaRepository<PolygonContinue, String> {
+}
---
+++ src/main/java/app/bpartners/geojobs/repository/model/PolygonContinue.java
@@ -0,0 +1,29 @@
+package app.bpartners.geojobs.repository.model;
+
+import app.bpartners.geojobs.job.model.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.io.File;
+
+import static org.hibernate.type.SqlTypes.NAMED_ENUM;
+
+@Entity
+@Table(name = "polygon_continue")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PolygonContinue {
+
+    @Id
+    @Column(nullable = false,name = "bucket_key")
+    private String bucketKey;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(NAMED_ENUM)
+    private Status.ProgressionStatus status;
+}
