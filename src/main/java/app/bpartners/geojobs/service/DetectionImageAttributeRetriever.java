5390d3150dd5019f288ea397e6aa2b5449bacb84
Ryan Andriamahery <ryan@hei.school>
2025-08-22 12:17:51 +0300

    fix(DetectionImageAttributeRetriever): check if key exist on S3 before presigning URL

--- src/main/java/app/bpartners/geojobs/service/DetectionImageAttributeRetriever.java
+++ src/main/java/app/bpartners/geojobs/service/DetectionImageAttributeRetriever.java
@@ -1,6 +1,7 @@
 package app.bpartners.geojobs.service;
 
 import app.bpartners.geojobs.file.bucket.BucketComponent;
+import app.bpartners.geojobs.file.bucket.CustomBucketComponent;
 import app.bpartners.geojobs.repository.model.detection.Detection;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
@@ -10,17 +11,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DetectionImageAttributeRetriever implements Function<Detection, String> {
   private final BucketComponent bucketComponent;
+  private final CustomBucketComponent customBucketComponent;
 
   @Override
   public String apply(Detection detection) {
     var imageFileKey = detection.getImageFileKey();
     var polygonGeoJsonZone = detection.getPolygonGeoJsonZone();
     if (imageFileKey == null && polygonGeoJsonZone != null) {
-      try {
-        return bucketComponent.presign("zone_images/" + detection.getId() + ".jpg");
-      } catch (RuntimeException e) {
+      var bucketKey = "zone_images/" + detection.getId() + ".jpg";
+      if (customBucketComponent.listObjects(bucketComponent.getBucketName(), bucketKey).isEmpty()) {
         return null;
       }
+      return bucketComponent.presign(bucketKey);
     }
     return imageFileKey == null ? null : bucketComponent.presign(imageFileKey);
   }
