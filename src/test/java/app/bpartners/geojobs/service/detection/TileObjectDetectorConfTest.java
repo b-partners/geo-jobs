71b59b11c29d1848886225daff314ca083e4a8d1
Daniel Langio <langio.tehiniavo@gmail.com>
2025-09-04 11:38:29 +0300

    chore(TileObjectDetectorConfIT): add JSON format check in TileObjectDetectorConfIT

--- src/test/java/app/bpartners/geojobs/service/detection/TileObjectDetectorConfTest.java
+++ src/test/java/app/bpartners/geojobs/service/detection/TileObjectDetectorConfTest.java
@@ -6,16 +6,20 @@ import static org.mockito.Mockito.when;
 
 import app.bpartners.geojobs.conf.FacadeIT;
 import app.bpartners.geojobs.file.bucket.BucketComponent;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.io.File;
-import java.nio.file.Files;
-
 class TileObjectDetectorConfIT extends FacadeIT {
+  ObjectMapper jsonMapper = new ObjectMapper();
+
   @Autowired TileObjectDetectorConf subject;
 
   @MockBean BucketComponent bucketComponentMock;
@@ -26,7 +30,17 @@ class TileObjectDetectorConfIT extends FacadeIT {
   void setUp() {
 
     mockFile = File.createTempFile("test", ".json");
-    String mockContent = "{\"urls\": [\"http://mock-api-url.com\"]}";
+    String mockContent =
+        """
+        [{
+            "objectType": "PISCINE",
+            "url": "dummy"
+          },
+          {
+            "objectType": "PASSAGE_PIETON",
+            "url": "dummy"
+          }]
+        """;
     Files.writeString(mockFile.toPath(), mockContent);
   }
 
@@ -38,5 +52,11 @@ class TileObjectDetectorConfIT extends FacadeIT {
 
     assertNotNull(actual);
     assertFalse(actual.isBlank());
+    AtomicReference<JsonNode> content = new AtomicReference<>();
+    assertDoesNotThrow(
+        () -> {
+          content.set(jsonMapper.readTree(actual));
+        });
+    assertTrue(content.get().get(0).has("url") && content.get().get(0).has("objectType"));
   }
 }
