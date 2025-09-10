573b979d8529526d9c7d03d5507c4903ea686b52
Daniel Langio <langio.tehiniavo@gmail.com>
2025-09-10 13:43:01 +0300

    fix(TileObjectDetectorConf): spring context loading timeout

--- src/main/java/app/bpartners/geojobs/service/detection/TileObjectDetectorConf.java
+++ src/main/java/app/bpartners/geojobs/service/detection/TileObjectDetectorConf.java
@@ -4,20 +4,25 @@ import app.bpartners.geojobs.file.bucket.BucketComponent;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
 @Slf4j
 public class TileObjectDetectorConf {
   private final BucketComponent bucketComponent;
-  private final String tileDetectionApiUrls;
+  private String tileDetectionApiUrls;
 
   public TileObjectDetectorConf(BucketComponent bucketComponent) {
     this.bucketComponent = bucketComponent;
-    this.tileDetectionApiUrls = getFromS3();
+    this.tileDetectionApiUrls = null;
+  }
+
+  public String getTileDetectionApiUrls() {
+    if (this.tileDetectionApiUrls == null) {
+      this.tileDetectionApiUrls = getFromS3();
+    }
+    return this.tileDetectionApiUrls;
   }
 
   public String getFromS3() {
