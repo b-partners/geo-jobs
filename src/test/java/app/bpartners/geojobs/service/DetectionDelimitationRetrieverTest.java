4410027fb376e1e0bb009da9762ff3f05c0106d6
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2025-09-18 12:11:00 +0300

    fix: ensure featureWithDelimitation is always multipolygon

--- src/test/java/app/bpartners/geojobs/service/DetectionDelimitationRetrieverTest.java
+++ src/test/java/app/bpartners/geojobs/service/DetectionDelimitationRetrieverTest.java
@@ -1,8 +1,12 @@
 package app.bpartners.geojobs.service;
 
+import static app.bpartners.geojobs.endpoint.rest.controller.mapper.FeatureMapper.toDomainFeature;
+import static app.bpartners.geojobs.endpoint.rest.controller.mapper.FeatureMapper.toRestFeature;
 import static app.bpartners.geojobs.endpoint.rest.model.Detection.GeoJsonDelimitationTypeEnum.ROOF;
 import static app.bpartners.geojobs.endpoint.rest.model.Detection.GeoJsonDelimitationTypeEnum.ZONE;
-import static java.util.UUID.randomUUID;
+import static app.bpartners.geojobs.endpoint.rest.model.Geometry.TypeEnum.MULTI_POLYGON;
+import static app.bpartners.geojobs.endpoint.rest.model.Geometry.TypeEnum.POLYGON;
+import static app.bpartners.geojobs.endpoint.rest.model.ModelName.TOITURE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -29,21 +33,25 @@ class DetectionDelimitationRetrieverTest {
   @Test
   void feature_with_delimitation_from_provided_geoJson_when_geoJsonDelimitation_type_is_roof()
       throws JsonProcessingException {
-    var providedGeoJson = oneFeature();
+    var providedGeoJson = multiPolygonFeature();
     var detection = detection(List.of(providedGeoJson), ROOF);
 
     when(detectionRepositoryMock.save(any())).thenReturn(detection);
 
     subject.accept(detection);
 
+    var featureWithDelimitationAsString = featureWithGeometryAsString(providedGeoJson);
     var expectedDetectionToBeSaved =
         detection.toBuilder()
             .pointDelimitation(
                 new HashMap<>(
                     Map.of(
-                        new ObjectMapper().writeValueAsString(providedGeoJson), providedGeoJson)))
+                        new ObjectMapper().writeValueAsString(featureWithDelimitationAsString),
+                        featureWithDelimitationAsString)))
             .featureWithDelimitations(
-                List.of(new FeatureWithDelimitation(providedGeoJson, List.of(providedGeoJson))))
+                List.of(
+                    new FeatureWithDelimitation(
+                        providedGeoJson, List.of(featureWithDelimitationAsString))))
             .build();
 
     verify(detectionRepositoryMock, times(1)).save(expectedDetectionToBeSaved);
@@ -51,7 +59,7 @@ class DetectionDelimitationRetrieverTest {
 
   @Test
   void building_api_should_be_called_when_geoJsonDelimitation_type_is_zone() {
-    var providedGeoJson = oneFeature();
+    var providedGeoJson = polygonFeature();
     var detection = detection(List.of(providedGeoJson), ZONE);
 
     when(detectionRepositoryMock.save(any())).thenReturn(detection);
@@ -64,7 +72,7 @@ class DetectionDelimitationRetrieverTest {
 
   @Test
   void building_api_should_be_called_when_geoJsonDelimitation_type_is_null() {
-    var providedGeoJson = oneFeature();
+    var providedGeoJson = polygonFeature();
     var detection = detection(List.of(providedGeoJson), null);
 
     when(detectionRepositoryMock.save(any())).thenReturn(detection);
@@ -75,23 +83,37 @@ class DetectionDelimitationRetrieverTest {
     verify(geometryConverterMock, times(1)).retrieveRoofPolygonsFrom(any());
   }
 
-  private static Detection detection(
-      List<Feature> providedGeoJson, GeoJsonDelimitationTypeEnum delimitationType) {
-    return Detection.builder()
-        .id("detectionId")
-        .detectableObjectModel(new DetectableObjectModel().modelName(ModelName.TOITURE))
-        .providedGeoJsonZone(providedGeoJson)
-        .geoJsonDelimitationType(delimitationType)
-        .build();
+  @Test
+  void polygon_is_mapped_to_multipolygon_when_delimitation_type_is_roof() {
+    var polygonFeature = polygonFeature();
+    var detection = detection(List.of(polygonFeature), ROOF);
+
+    when(detectionRepositoryMock.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+    subject.accept(detection);
+
+    verify(detectionRepositoryMock)
+        .save(
+            argThat(
+                savedDetection -> {
+                  var fwd = savedDetection.getFeatureWithDelimitations().getFirst();
+                  var delimitations = fwd.delimitations();
+                  if (delimitations.size() != 1) {
+                    return false;
+                  }
+
+                  var delimitation = delimitations.getFirst();
+                  return MULTI_POLYGON.equals(delimitation.getGeometry().getGeometryType());
+                }));
   }
 
-  private static Feature oneFeature() {
+  private static Feature polygonFeature() {
     return Feature.builder()
-        .id(randomUUID().toString())
+        .properties(new HashMap<>())
         .zoom(20)
         .geometry(
             Feature.FeatureGeometry.builder()
-                .geometryType(Geometry.TypeEnum.POLYGON)
+                .geometryType(POLYGON)
                 .actualInstanceStringValue(
                     """
                     {
@@ -108,5 +130,49 @@ class DetectionDelimitationRetrieverTest {
                     """)
                 .build())
         .build();
+  }
+
+  private static Feature multiPolygonFeature() {
+    return Feature.builder()
+        .id(null)
+        .zoom(20)
+        .properties(new HashMap<>())
+        .geometry(
+            Feature.FeatureGeometry.builder()
+                .geometryType(MULTI_POLYGON)
+                .actualInstanceStringValue(
+                    """
+                    {
+                      "type": "MultiPolygon",
+                      "coordinates": [
+                        [
+                          [
+                            [0.0, 0.0],
+                            [10.0, 0.0],
+                            [5.0, 10.0],
+                            [0.0, 0.0]
+                          ]
+                        ]
+                      ]
+                    }
+                    """)
+                .build())
+        .build();
+  }
+
+  private static Detection detection(
+      List<Feature> providedGeoJson, GeoJsonDelimitationTypeEnum delimitationType) {
+    return Detection.builder()
+        .id("detectionId")
+        .detectableObjectModel(new DetectableObjectModel().modelName(TOITURE))
+        .providedGeoJsonZone(providedGeoJson)
+        .geoJsonDelimitationType(delimitationType)
+        .build();
+  }
+
+  private static Feature featureWithGeometryAsString(Feature feature) {
+    var restFeature = toRestFeature(feature);
+    restFeature.putPropertiesItem("zoom", feature.getZoom());
+    return toDomainFeature(restFeature);
   }
 }
