176a83fe59594e8b395f17eb2cd20e131491e7bf
Daniel Langio <langio.tehiniavo@gmail.com>
2025-10-02 16:47:06 +0300

    fix(db.migration): detection_step_name type migration

--- src/main/resources/db/migration/V43_122__Update_detection_step_name_type.sql
+++ src/main/resources/db/migration/V43_122__Update_detection_step_name_type.sql
@@ -13,32 +13,9 @@ $$
                 );
         END IF;
 
-        -- 2. Corrects old values
-        UPDATE detection_step
-        SET name = 'POST_PROCESSING'
-        WHERE name IN ('HUMAN_DETECTION', 'GEO_JSON_CONVERSION');
-
-        -- 3. Verifies if the column is in the old type
-        IF EXISTS (
-            SELECT 1
-            FROM information_schema.columns
-            WHERE table_name = 'detection_step'
-              AND column_name = 'name'
-              AND udt_name = 'detection_step_name'
-        ) THEN
-            ALTER TABLE detection_step
-                ALTER COLUMN name TYPE detection_step_name_new
-                    USING name::text::detection_step_name_new;
-        END IF;
-
-        -- 4. Deletes the old type if it exists
         IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'detection_step_name') THEN
-            DROP TYPE detection_step_name;
-        END IF;
-
-        -- 5. Renames the new type same as the old one
-        IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'detection_step_name_new') THEN
-            ALTER TYPE detection_step_name_new RENAME TO detection_step_name;
+            ALTER TYPE detection_step_name ADD VALUE 'REQUEST_ACCEPTED';
+            ALTER TYPE detection_step_name ADD VALUE 'POST_PROCESSING';
         END IF;
     END
 $$;
