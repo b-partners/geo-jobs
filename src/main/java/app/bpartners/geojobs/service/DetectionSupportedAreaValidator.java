bafeca70c4b703d8650d233767b51e3322e7de3a
Ryan Andriamahery <ryan@hei.school>
2025-10-13 19:30:36 +0300

    fix(DetectionSupportedAreaValidator): accumulate provided features

--- src/main/java/app/bpartners/geojobs/service/DetectionSupportedAreaValidator.java
+++ src/main/java/app/bpartners/geojobs/service/DetectionSupportedAreaValidator.java
@@ -15,6 +15,7 @@ import java.util.Objects;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -54,19 +55,29 @@ public class DetectionSupportedAreaValidator implements Consumer<Detection> {
       return;
     }
     var providedZoneGeometries = unifiedProvidedPolygon.getNumGeometries();
+    var accumulatedArea = 0.0;
     for (int i = 0; i < providedZoneGeometries; i++) {
       var providedPolygonGeometry = unifiedProvidedPolygon.getGeometryN(i);
       var computedArea = geometrySquareMeterArea.apply(providedPolygonGeometry);
       log.info("computed area {} m^2", Math.round(computedArea));
       if (computedArea > ONE_KILOMETER_SQUARE_DEGREE) {
-        throw new UnsupportedDetectionAreaException(
-            "Provided zone contains geometry "
-                + providedPolygonGeometry.toText()
-                + " over 1 kilometer square degree area :"
-                + BigDecimal.valueOf(computedArea).setScale(0, UP).longValue()
-                + " m^2",
-            computedArea);
+        throwUnsupportedDetectionAreaException(providedPolygonGeometry, computedArea);
       }
+      accumulatedArea += computedArea;
     }
+    if (accumulatedArea > ONE_KILOMETER_SQUARE_DEGREE) {
+      throwUnsupportedDetectionAreaException(unifiedProvidedPolygon, accumulatedArea);
+    }
+  }
+
+  private void throwUnsupportedDetectionAreaException(
+      Geometry providedPolygonGeometry, Double computedArea) {
+    throw new UnsupportedDetectionAreaException(
+        "Provided zone contains geometry "
+            + providedPolygonGeometry.toText()
+            + " over 1 kilometer square degree area :"
+            + BigDecimal.valueOf(computedArea).setScale(0, UP).longValue()
+            + " m^2",
+        computedArea);
   }
 }
