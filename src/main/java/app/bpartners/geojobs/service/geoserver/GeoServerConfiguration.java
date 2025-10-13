36589378bbe32d799fe75bf652e3fd07de5b1453
Ryan Andriamahery <ryan@hei.school>
2025-10-13 20:09:40 +0300

    fix(GeoServerConfiguration): handle layer null

--- src/main/java/app/bpartners/geojobs/service/geoserver/GeoServerConfiguration.java
+++ src/main/java/app/bpartners/geojobs/service/geoserver/GeoServerConfiguration.java
@@ -15,7 +15,9 @@ public class GeoServerConfiguration {
 
   public GeoServerProperties defaultGeoServerProperties(String layer) {
     var overrideLayer =
-        layer.contains("Auvergne_Rhone_Alpes") ? "Auvergne_Rhone_Alpes_PCRS_5cm" : layer;
+        layer == null
+            ? layer
+            : layer.contains("Auvergne_Rhone_Alpes") ? "Auvergne_Rhone_Alpes_PCRS_5cm" : layer;
     return new GeoServerProperties()
         .geoServerUrl(url)
         .geoServerParameter(
