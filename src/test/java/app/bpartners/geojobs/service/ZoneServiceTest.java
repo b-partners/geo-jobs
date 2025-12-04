8f811ea354780a712480bd13f620b217af27bb81
Daniel Langio <langio.tehiniavo@gmail.com>
2025-12-04 16:18:33 +0300

    fix(ZoneServiceTest): introduce detectableObjectModelList into Detection related test

--- src/test/java/app/bpartners/geojobs/service/ZoneServiceTest.java
+++ src/test/java/app/bpartners/geojobs/service/ZoneServiceTest.java
@@ -925,7 +925,11 @@ class ZoneServiceTest {
         detectionCreator.create(
             detectionId, randomUUID().toString(), randomUUID().toString(), null);
     var addresses = List.of("11-7 Rue Mot, 94120 Fontenay-sous-Bois, France");
-    var expected = detection.toBuilder().convertedAddresses(addresses).build();
+    var expected =
+        detection.toBuilder()
+            .detectableObjectModelList(List.of())
+            .convertedAddresses(addresses)
+            .build();
     when(detectionRepositoryMock.findByEndToEndIdAndCommunityOwnerId(any(), any()))
         .thenReturn(Optional.of(detection));
     when(detectionRepositoryMock.save(any()))
@@ -944,6 +948,7 @@ class ZoneServiceTest {
             .geoJsonZone(null)
             .geoServerProperties(detection.getGeoServerProperties())
             .detectableObjectModel(detection.getDetectableObjectModel())
+            .detectableObjectModelList(detection.getDetectableObjectModelList())
             .step(
                 new DetectionStep()
                     .name(REQUEST_ACCEPTED)
@@ -996,6 +1001,7 @@ class ZoneServiceTest {
             .geoJsonZone(null)
             .geoServerProperties(detection.getGeoServerProperties())
             .detectableObjectModel(detection.getDetectableObjectModel())
+            .detectableObjectModelList(detection.getDetectableObjectModelList())
             .step(
                 new DetectionStep()
                     .name(REQUEST_ACCEPTED)
@@ -1052,6 +1058,7 @@ class ZoneServiceTest {
             .geoJsonZone(detection.getProvidedGeoJsonZone())
             .geoServerProperties(detection.getGeoServerProperties())
             .detectableObjectModel(detection.getDetectableObjectModel())
+            .detectableObjectModelList(detection.getDetectableObjectModelList())
             .step(
                 new DetectionStep()
                     .name(REQUEST_ACCEPTED)
@@ -1121,6 +1128,7 @@ class ZoneServiceTest {
             .geoJsonZone(featureCreator.defaultFeatures())
             .geoServerProperties(detection.getGeoServerProperties())
             .detectableObjectModel(detection.getDetectableObjectModel())
+            .detectableObjectModelList(detection.getDetectableObjectModelList())
             .step(
                 new DetectionStep()
                     .name(REQUEST_ACCEPTED)
