011329c012a69316f13d469850730e289af96bad
RickaPrincy <rckprincy@gmail.com>
2025-12-09 08:02:14 +0300

    chore(Lidar): add lidar api url fallback for deprecated and unavailable data

--- src/main/java/app/bpartners/geojobs/service/lidar/api/LidarApi.java
+++ src/main/java/app/bpartners/geojobs/service/lidar/api/LidarApi.java
@@ -1,14 +1,17 @@
 package app.bpartners.geojobs.service.lidar.api;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toSet;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geom.Geometry;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -18,28 +21,29 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class LidarApi {
   private final LidarApiConf conf;
   private final RestTemplate restTemplate;
+  private final LidarApiFallbackUrl fallbackUrl;
+  private static final String UPDATED_DATA_PREFIX = "https://data.geopf.fr/";
   private static final Set<String> ALLOWED_URL_PREFIXES =
       Set.of(
-          "https://storage.sbg.cloud.ovh.net/",
-          "https://lidar.data.gouv.fr/",
-          "https://data.geopf.fr/");
+          "https://storage.sbg.cloud.ovh.net/", "https://lidar.data.gouv.fr/", UPDATED_DATA_PREFIX);
+
+  private static final long UPDATED_VALID_DATA = 50_000_000;
 
   public Map<String, Set<Geometry>> getUniqueLidarFilesUrls(Collection<Geometry> geometries) {
     Map<String, Set<Geometry>> filesUrls = new HashMap<>();
 
     for (var geometry : geometries) {
-      var uriBuilder = UriComponentsBuilder.fromHttpUrl(conf.getUrl());
-      conf.getDefaultParams(geometry.getEnvelopeInternal()).forEach(uriBuilder::queryParam);
+      var envelope = geometry.getEnvelopeInternal();
+      var features = getFeatures(envelope);
+      var urls = getUrlsFromFeatures(features);
 
-      var features =
-          requireNonNull(
-                  restTemplate
-                      .getForEntity(uriBuilder.toUriString(), FeatureCollection.class)
-                      .getBody())
-              .getFeatures();
+      if (urls.isEmpty()) {
+        urls = fallbackUrl.getUniqueLidarUrlsForUnavailableData(envelope);
+      } else {
+        urls = handleDeprecatedData(envelope, urls);
+      }
 
-      for (var feature : features) {
-        var url = feature.getProperties().get("url").toString();
+      for (var url : urls) {
         log.info("LAZ File to download: {}", url);
         filesUrls.computeIfAbsent(url, key -> new HashSet<>()).add(geometry);
       }
@@ -66,14 +70,58 @@ public class LidarApi {
         outputStream.write(data);
       }
 
+      log.info("Finished downloading {}", fileUrl);
       return Optional.of(tempFile);
+    } catch (HttpClientErrorException.NotFound e) {
+      log.warn("File not found (404): {}", fileUrl);
+      return Optional.empty();
     } catch (Exception e) {
       log.error("Failed to download LAZ file from {}", fileUrl, e);
       throw new RuntimeException("Could not download file: " + fileUrl, e);
     }
   }
 
-  public static boolean isSafeUrl(String url) {
+  private Set<String> handleDeprecatedData(Envelope envelope, Set<String> urls) {
+    if (urls.stream().allMatch(url -> url.startsWith(UPDATED_DATA_PREFIX))) {
+      return urls;
+    }
+
+    var hasValidContent = urls.stream().allMatch(this::hasValidContent);
+    if (hasValidContent) {
+      return urls;
+    }
+
+    return fallbackUrl.getUniqueLidarUrlsForDeprecatedData(envelope);
+  }
+
+  private boolean hasValidContent(String url) {
+    try {
+      var headers = restTemplate.headForHeaders(url);
+      long contentLength = headers.getContentLength();
+      log.info("Content-Length={} for fileUrl={}", contentLength, url);
+      return contentLength > UPDATED_VALID_DATA;
+    } catch (Exception e) {
+      log.warn("Cannot get the Content-Length of the fileUrl={}", url);
+      return true;
+    }
+  }
+
+  @SuppressWarnings("all")
+  private List<FeatureCollection.Feature> getFeatures(Envelope envelope) {
+    var uriBuilder = UriComponentsBuilder.fromHttpUrl(conf.getUrl());
+    conf.getDefaultParams(envelope).forEach(uriBuilder::queryParam);
+    return requireNonNull(
+            restTemplate.getForEntity(uriBuilder.toUriString(), FeatureCollection.class).getBody())
+        .getFeatures();
+  }
+
+  private static Set<String> getUrlsFromFeatures(List<FeatureCollection.Feature> features) {
+    return features.stream()
+        .map(feature -> feature.getProperties().get("url").toString())
+        .collect(toSet());
+  }
+
+  private static boolean isSafeUrl(String url) {
     if (url == null) return false;
     return ALLOWED_URL_PREFIXES.stream().anyMatch(url::startsWith);
   }
