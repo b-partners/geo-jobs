fed701827040ef1166f05c29f88e7ad3a8dfaec8
Ryan Andriamahery <ryan@hei.school>
2026-01-28 20:14:32 +0300

    fix(UserApiKey): ignore unknown properties

--- src/main/java/app/bpartners/geojobs/service/dashboard/component/UserApiKey.java
+++ src/main/java/app/bpartners/geojobs/service/dashboard/component/UserApiKey.java
@@ -1,3 +1,6 @@
 package app.bpartners.geojobs.service.dashboard.component;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record UserApiKey(String key) {}
