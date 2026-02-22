d97aa25610129f205c276fa64deb6a70d652ff89
Ryan Andriamahery <ryan@hei.school>
2026-02-22 23:37:37 +0300

    chore(ThreeDResponseStatus): return default supported values

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/cityjson/CityJSONRequestMapper.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/mapper/cityjson/CityJSONRequestMapper.java
@@ -1,6 +1,7 @@
 package app.bpartners.geojobs.endpoint.rest.controller.mapper.cityjson;
 
 import static app.bpartners.geojobs.endpoint.rest.model.DelimitationObjectType.BUILDING_ROOF;
+import static app.bpartners.geojobs.endpoint.rest.model.DelimitationType.PARCEL_FREE_DELIMITATION;
 import static java.time.Instant.now;
 
 import app.bpartners.geojobs.endpoint.rest.controller.mapper.FeatureMapper;
@@ -65,6 +66,8 @@ public class CityJSONRequestMapper {
         .id(cityJSONRequest.getId())
         .delimitations(restDelimitations)
         .status(CityJSONRequestStatusMapper.toGenericStatusRest(cityJSONRequest.getStatus()))
+        .delimitationObjectType(BUILDING_ROOF)
+        .delimitationType(PARCEL_FREE_DELIMITATION)
         .cityJsonFileUrls(restCityJsons);
   }
 
