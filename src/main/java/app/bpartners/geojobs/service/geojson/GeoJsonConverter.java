0b33f0ddfec3ef9e0687f8b5f72a2b8ac65ff868
Daniel Langio <langio.tehiniavo@gmail.com>
2026-02-27 11:32:47 +0300

    feat: overlapping same class detections without splitting

--- src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonConverter.java
+++ src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonConverter.java
@@ -2,8 +2,12 @@ package app.bpartners.geojobs.service.geojson;
 
 import static app.bpartners.geojobs.service.geojson.GeoJson.fromFeatures;
 
+import app.bpartners.geojobs.endpoint.rest.postprocessing.DetectionBoundaryMerger;
+import app.bpartners.geojobs.endpoint.rest.postprocessing.model.LatLonPolygon;
+import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
 import app.bpartners.geojobs.model.DetectedTile;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class GeoJsonConverter implements Converter<List<DetectedTile>, GeoJson> {
   private static final int DEFAULT_IMAGE_SIZE = 1024;
   private final GeoJsonMapper mapper;
+  private final DetectionBoundaryMerger merger;
 
   @Override
   public GeoJson convert(List<DetectedTile> detectedTiles) {
@@ -29,6 +34,14 @@ public class GeoJsonConverter implements Converter<List<DetectedTile>, GeoJson> 
                 })
             .flatMap(List::stream)
             .toList();
-    return fromFeatures(geoFeatures);
+
+    var toUnify =
+        geoFeatures.stream()
+            .map(f -> LatLonPolygon.latLon(f).tiledPolygon(TilingConf.getDefaultInstance()))
+            .collect(Collectors.toSet());
+
+    var unified = merger.apply(toUnify).stream().map(LatLonPolygon::toGeoFeature).toList();
+
+    return fromFeatures(unified);
   }
 }
