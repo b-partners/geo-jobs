8973b646203dd3736949c10f8eeb87d448bb5111
Ryan Andriamahery <ryan@hei.school>
2026-03-27 19:37:04 +0300

    fix(DetectionRoofPropertiesRequested): produces DetectionRoofPropertiesRequested handled with EVENT_STACK_2 when images output not needed

--- src/main/java/app/bpartners/geojobs/endpoint/event/model/DetectionRoofPropertiesRequested.java
+++ src/main/java/app/bpartners/geojobs/endpoint/event/model/DetectionRoofPropertiesRequested.java
@@ -1,5 +1,6 @@
 package app.bpartners.geojobs.endpoint.event.model;
 
+import static app.bpartners.geojobs.endpoint.event.EventStack.EVENT_STACK_2;
 import static app.bpartners.geojobs.endpoint.event.EventStack.EVENT_STACK_4;
 
 import app.bpartners.geojobs.endpoint.event.EventStack;
@@ -27,6 +28,6 @@ public class DetectionRoofPropertiesRequested extends PojaEvent {
 
   @Override
   public EventStack getEventStack() {
-    return EVENT_STACK_4;
+    return EVENT_STACK_2;
   }
 }
