b9f9d88a069e6e0dba3327926bc7cbdfe1acc4c6
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2026-04-13 10:11:58 +0300

    chore(CityJSON): improved lr simplifier
    * wip: feat: lr simplification
    
    * refactor: simplify lr simplifier
    
    * feat: integration of lr to plane delimitation

--- src/test/java/app/bpartners/geojobs/utils/lidar/Plane3DExtractorConfCreator.java
+++ src/test/java/app/bpartners/geojobs/utils/lidar/Plane3DExtractorConfCreator.java
@@ -10,7 +10,8 @@ public class Plane3DExtractorConfCreator {
             RangedConf.from(
                 new RangedConf.IntegerRangedConf<>(Integer.MIN_VALUE, 200, 0.2),
                 new RangedConf.IntegerRangedConf<>(201, Integer.MAX_VALUE, 0.2)))
-        .simplificationEpsilon(0.5)
+        .dpsEpsilon(0.5)
+        .lrDegEpsilon(5)
         .build();
   }
 }
