1a7924203a07794133e44c93627e5603273e5ff9
Dinasoa <hei.dinasoa@gmail.com>
2026-04-13 17:55:09 +0300

    fix: add bounding box pre-check before Swiss geometry containment test and format code

--- src/test/java/app/bpartners/geojobs/endpoint/rest/controller/DetectionControllerIT.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/controller/DetectionControllerIT.java
@@ -278,7 +278,8 @@ class DetectionControllerIT extends FacadeIT {
     var statistic = taskStatisticCreator.createProcessingTask(zoneDetectionJob.getId(), DETECTION);
     when(zoneDetectionJobService.getTaskStatistic(any(String.class))).thenReturn(statistic);
 
-    var actual = subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, null);
+    var actual =
+        subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, null);
 
     var expected =
         new app.bpartners.geojobs.endpoint.rest.model.Detection()
@@ -309,32 +310,37 @@ class DetectionControllerIT extends FacadeIT {
         zoneDetectionJobRepository.save(
             zoneDetectionJob(randomUUID().toString(), zoneTilingJob.getId()));
     var zoneName = "Random address";
-        detectionRepository.save(
-            detectionCreator
-                .createFromZTJAndZDJ(zoneTilingJob.getId(), zoneDetectionJob.getId())
-                .toBuilder()
-                .isOutputZipped(true)
-                    .zoneName(zoneName)
-                .build());
-        detectionRepository.save(
-            detectionCreator
-                .createFromZTJAndZDJ(zoneTilingJob2.getId(), zoneDetectionJob2.getId())
-                .toBuilder()
-                .isOutputZipped(true)
-                .zoneName(randomUUID().toString())
-                .build());
+    detectionRepository.save(
+        detectionCreator
+            .createFromZTJAndZDJ(zoneTilingJob.getId(), zoneDetectionJob.getId())
+            .toBuilder()
+            .isOutputZipped(true)
+            .zoneName(zoneName)
+            .build());
+    detectionRepository.save(
+        detectionCreator
+            .createFromZTJAndZDJ(zoneTilingJob2.getId(), zoneDetectionJob2.getId())
+            .toBuilder()
+            .isOutputZipped(true)
+            .zoneName(randomUUID().toString())
+            .build());
     var statistic = taskStatisticCreator.createProcessingTask(zoneDetectionJob.getId(), DETECTION);
     when(zoneDetectionJobService.getTaskStatistic(any(String.class))).thenReturn(statistic);
 
-    var actualFiltered = subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, "Addre");
-    var actual = subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null,  null);
+    var actualFiltered =
+        subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, "Addre");
+    var actual =
+        subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, null);
 
     assertTrue(actualFiltered.size() < actual.size());
     assertTrue(actual.containsAll(actualFiltered));
-    assertTrue(actualFiltered.stream().allMatch(d -> {
-        assertNotNull(d.getZoneName());
-        return d.getZoneName().equals(zoneName);
-    }));
+    assertTrue(
+        actualFiltered.stream()
+            .allMatch(
+                d -> {
+                  assertNotNull(d.getZoneName());
+                  return d.getZoneName().equals(zoneName);
+                }));
   }
 
   @Test
@@ -344,7 +350,8 @@ class DetectionControllerIT extends FacadeIT {
         detectionRepository.save(
             detectionWithoutZdj(zoneTilingJob.getId(), featureCreator.defaultFeatures()));
 
-    var actual = subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, null);
+    var actual =
+        subject.getDetections(new PageFromOne(1), new BoundedPageSize(10), null, null, null);
 
     var expected =
         new app.bpartners.geojobs.endpoint.rest.model.Detection()
@@ -385,7 +392,8 @@ class DetectionControllerIT extends FacadeIT {
                     .endToEndId(randomUUID().toString())
                     .build());
 
-    var actualList = subject.getDetections(new PageFromOne(1), new BoundedPageSize(1), null, null, null);
+    var actualList =
+        subject.getDetections(new PageFromOne(1), new BoundedPageSize(1), null, null, null);
 
     assertEquals(detection.getEndToEndId(), actualList.getFirst().getId());
   }
