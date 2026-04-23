730a3f0ecdc7fd9399554cb94b849b53d73c6bd9
Daniel Langio <langio.tehiniavo@gmail.com>
2026-04-23 11:25:57 +0300

    Revert "chore(db.migration): add api key related community authorization indexes"
    This reverts commit 0eed7dc7

--- src/main/resources/db/migration/V43_150__Add_api_key_related_community_owner_indexes.sql
+++
@@ -1,5 +0,0 @@
-create index if not exists community_authorization_api_key_community_owner_id_idx
-    on community_authorization_api_key (id_community_authorization_owner);
-
-create index if not exists revoked_api_key_community_owner_id_idx
-    on revoked_api_key (community_owner_id);
