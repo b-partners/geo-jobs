a8000ffb55ae487059347650cb5554787c547560
Daniel Langio <langio.tehiniavo@gmail.com>
2026-05-15 14:29:05 +0300

    chore(CityJSONTexture): delete deprecated column

---
+++ src/main/resources/db/migration/V43_156__Drop_deprecated_texture_rows.sql
@@ -0,0 +1,7 @@
+alter table city_json_texture
+drop column top_left_longitude,
+drop column top_left_latitude,
+drop column pixel_width,
+drop column pixel_height,
+drop column shear_x,
+drop column shear_y;
