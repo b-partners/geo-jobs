b68320e4360cb9067ba02cda3bcf9b25f8b878f8
Ryan Andriamahery <ryan@hei.school>
2026-06-19 12:34:07 +0300

    fix(GeoJsonConverter): intersect converted objects with provided geometry

--- src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonConverter.java
+++ src/main/java/app/bpartners/geojobs/service/geojson/GeoJsonConverter.java
@@ -1,7 +1,7 @@
 package app.bpartners.geojobs.service.geojson;
 
 import static app.bpartners.geojobs.endpoint.rest.postprocessing.BoundaryMerger.invert;
-import static app.bpartners.geojobs.repository.model.detection.DetectableType.*;
+import static app.bpartners.geojobs.model.geometry.GeometryFactory.geometryFactory;
 import static app.bpartners.geojobs.service.geojson.GeoJson.fromFeatures;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.DetectionBoundaryMerger;
@@ -9,43 +9,69 @@ import app.bpartners.geojobs.endpoint.rest.postprocessing.model.LatLonPolygon;
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
 import app.bpartners.geojobs.model.ConversionFormatType;
 import app.bpartners.geojobs.model.DetectedTile;
-import app.bpartners.geojobs.repository.model.detection.DetectedObject;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.core.convert.converter.Converter;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.geom.Polygon;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @AllArgsConstructor
-public class GeoJsonConverter implements Converter<List<DetectedTile>, GeoJson> {
+public class GeoJsonConverter implements BiFunction<List<DetectedTile>, MultiPolygon, GeoJson> {
   private static final int DEFAULT_IMAGE_SIZE = 1024;
   private final GeoJsonMapper mapper;
   private final DetectionBoundaryMerger merger;
+  private final GeometryConverter geometryConverter;
 
   @Override
-  public GeoJson convert(List<DetectedTile> detectedTiles) {
-    var detectableTypes =
-        detectedTiles.stream()
-            .flatMap(tile -> tile.getDetectedObjects().stream())
-            .map(DetectedObject::getDetectableObjectType)
-            .collect(Collectors.toSet());
+  public GeoJson apply(
+      List<DetectedTile> detectedTiles, MultiPolygon providedGeometryMultiPolygon) {
     List<GeoJson.GeoFeature> geoFeatures =
         detectedTiles.stream()
             .map(
                 detectedTile -> {
                   var tile = detectedTile.getTile();
-                  var xTile = tile.getCoordinates().getX();
-                  var yTile = tile.getCoordinates().getY();
-                  var zoom = tile.getCoordinates().getZ();
+                  var tileCoordinates = tile.getCoordinates();
+                  var xTile = tileCoordinates.getX();
+                  var yTile = tileCoordinates.getY();
+                  var zoom = tileCoordinates.getZ();
                   return mapper.toGeoFeatures(
                       xTile, yTile, zoom, DEFAULT_IMAGE_SIZE, detectedTile.getDetectedObjects());
                 })
             .flatMap(List::stream)
             .toList();
+
+    if (providedGeometryMultiPolygon != null) {
+      geoFeatures.forEach(
+          geoFeature -> {
+            var multiPolygon = geometryConverter.apply(geoFeature.getGeometry().getCoordinates());
+            var detectedGeoFeatureInsideProvidedGeometry =
+                providedGeometryMultiPolygon.intersection(multiPolygon);
+            if (detectedGeoFeatureInsideProvidedGeometry
+                instanceof MultiPolygon multiPolygonInsideProvidedGeometry) {
+              geoFeature.setGeometry(
+                  geometryConverter.restMultiPolygonFromJts(multiPolygonInsideProvidedGeometry));
+            } else if (detectedGeoFeatureInsideProvidedGeometry
+                instanceof Polygon polygonInsideProvidedGeometry) {
+              var multiPolygonFromPolygon =
+                  geometryFactory.createMultiPolygon(new Polygon[] {polygonInsideProvidedGeometry});
+              geoFeature.setGeometry(
+                  geometryConverter.restMultiPolygonFromJts(multiPolygonFromPolygon));
+            } else {
+              log.error(
+                  "Unable to handle geometry intersection type {} provided geometry with"
+                      + " geoFeature.geometry : {}",
+                  detectedGeoFeatureInsideProvidedGeometry.getClass().getSimpleName(),
+                  geoFeature.getGeometry());
+            }
+          });
+    }
+
     return fromFeatures(geoFeatures);
   }
 
