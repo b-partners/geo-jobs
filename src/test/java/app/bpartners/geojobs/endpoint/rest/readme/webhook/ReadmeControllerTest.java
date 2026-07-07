da060bfee5d5a6057b3d1e8a0435201461bf4419
Ryan Andriamahery <ryan@hei.school>
2026-07-07 10:39:29 +0300

    chore: move non controller outside versioning

--- src/test/java/app/bpartners/geojobs/endpoint/rest/readme/webhook/ReadmeControllerTest.java
+++ src/test/java/app/bpartners/geojobs/endpoint/rest/readme/webhook/ReadmeControllerTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import app.bpartners.geojobs.endpoint.rest.controller.v1.ReadmeController;
+import app.bpartners.geojobs.endpoint.rest.controller.ReadmeController;
 import app.bpartners.geojobs.readme.webhook.ReadmeWebhookConf;
 import app.bpartners.geojobs.readme.webhook.ReadmeWebhookService;
 import app.bpartners.geojobs.readme.webhook.ReadmeWebhookValidator;
